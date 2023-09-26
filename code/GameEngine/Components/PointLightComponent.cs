93fe7a97250007626aa34aa03878efa19373b847
Garry Newman <garrynewman@users.noreply.github.com>
2023-09-26 20:08:25 +0100

    Point light, spot light

---
+++ code/GameEngine/Components/PointLightComponent.cs
@@ -0,0 +1,49 @@
+﻿using Sandbox;
+using Sandbox.Diagnostics;
+using System;
+
+[Title( "Point Light" )]
+[Category( "Light" )]
+[Icon( "light_mode", "red", "white" )]
+public class PointLightComponent : GameObjectComponent
+{
+	SceneLight _sceneObject;
+
+	[Property] public Color LightColor { get; set; } = "#E9FAFF";
+	[Property] public float Radius { get; set; } = 400;
+
+	public override void DrawGizmos()
+	{
+		//using var scope = Gizmo.Scope( $"light-{GetHashCode()}" );
+
+		//var fwd = Vector3.Forward;
+
+		//Gizmo.Draw.Color = LightColor;
+
+	}
+
+	public override void OnEnabled()
+	{
+		Assert.True( _sceneObject == null );
+		Assert.NotNull( Scene );
+
+		_sceneObject = new SceneLight( Scene.SceneWorld, GameObject.Transform.Position, Radius, LightColor );
+	}
+
+	public override void OnDisabled()
+	{
+		_sceneObject?.Delete();
+		_sceneObject = null;
+	}
+
+	protected override void OnPreRender()
+	{
+		if ( !_sceneObject.IsValid() )
+			return;
+
+		_sceneObject.Transform = GameObject.Transform;
+		_sceneObject.LightColor = LightColor;
+		_sceneObject.Radius = Radius;
+	}
+
+}
---
+++ code/GameEngine/Components/SpotLightComponent.cs
@@ -0,0 +1,69 @@
+﻿using Sandbox;
+using Sandbox.Diagnostics;
+using System;
+
+[Title( "Spot Light" )]
+[Category( "Light" )]
+[Icon( "light_mode", "red", "white" )]
+public class SpotLightComponent : GameObjectComponent
+{
+	SceneSpotLight _sceneObject;
+
+	[Property] public Color LightColor { get; set; } = "#E9FAFF";
+	[Property] public float Radius { get; set; } = 500;
+	[Property] public float ConeOuter { get; set; } = 45;
+	[Property] public float ConeInner { get; set; } = 15;
+	[Property] public float Attenuation { get; set; } = 1.0f;
+
+	public override void DrawGizmos()
+	{
+		using var scope = Gizmo.Scope( $"light-{GetHashCode()}" );
+
+		var fwd = Vector3.Forward;
+
+		Gizmo.Draw.Color = LightColor;
+
+	//	Gizmo.Transform = Transform.Zero;
+	//	Gizmo.Draw.Sprite( GameObject.Transform.Position, 10, Texture.Load( FileSystem.Mounted, "/editor/directional_light.png" ) );
+
+	}
+
+	public override void OnEnabled()
+	{
+		Assert.True( _sceneObject == null );
+		Assert.NotNull( Scene );
+
+		_sceneObject = new SceneSpotLight( Scene.SceneWorld, GameObject.Transform.Position, LightColor );
+		//_sceneObject.Transform = GameObject.WorldTransform;
+		//_sceneObject.ShadowsEnabled = true;
+	}
+
+	public override void OnDisabled()
+	{
+		_sceneObject?.Delete();
+		_sceneObject = null;
+	}
+
+	protected override void OnPreRender()
+	{
+		if ( !_sceneObject.IsValid() )
+			return;
+
+		_sceneObject.Transform = GameObject.Transform;
+	//	_sceneObject.ShadowsEnabled = Shadows;
+		_sceneObject.LightColor = LightColor;
+		_sceneObject.FallOff = 1;
+		_sceneObject.Radius = Radius;
+		_sceneObject.ConeInner = ConeInner;
+		_sceneObject.ConeOuter = ConeOuter;
+		_sceneObject.QuadraticAttenuation = Attenuation;
+		////_sceneObject.FallOff = 0.1f;
+		//_sceneObject.SkyColor = SkyColor;
+
+		//_sceneObject.ShadowCascadeCount = 3;
+		//_sceneObject.SetShadowCascadeDistance( 0, 200 );
+		//_sceneObject.SetShadowCascadeDistance( 1, 500 );
+		//_sceneObject.SetShadowCascadeDistance( 2, 5000 );
+	}
+
+}
