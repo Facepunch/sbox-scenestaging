b9ae259698740d69f8194a3a768c96a7c365d22d
Garry Newman <garrynewman@users.noreply.github.com>
2023-10-03 12:52:27 +0100

    Serialize GameObjects using their guid

--- code/GameEngine/GameObjectComponent.Serialize.cs
+++ code/GameEngine/GameObjectComponent.Serialize.cs
@@ -25,7 +25,19 @@ public abstract partial class GameObjectComponent
 
 		foreach( var prop in t.Properties.Where( x => x.HasAttribute<PropertyAttribute>() ).OrderBy( x => x.Name ) )
 		{
-			json.Add( prop.Name, Json.ToNode( prop.GetValue( this ) ) );
+			var value = prop.GetValue( this );
+
+			if ( prop.PropertyType == typeof(GameObject) )
+			{
+				if ( value is GameObject go )
+				{
+					// todo: if this is a prefab, not in the scene, we need to handle that too!
+					json.Add( prop.Name, go.Id );
+				}
+				continue;
+			}
+
+			json.Add( prop.Name, Json.ToNode( value ) );
 		}
 
 		return json;
