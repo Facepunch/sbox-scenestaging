03ba48ad4e76288da913efad3559833a6a1aca54
Garry Newman <garrynewman@users.noreply.github.com>
2023-10-08 07:19:04 +0100

    Add CameraPhysicsDebug component

---
+++ code/CameraPhysicsDebug.cs
@@ -0,0 +1,71 @@
+using Sandbox;
+using System.Collections.Generic;
+
+public sealed class CameraPhysicsDebug : BaseComponent
+{
+	[Property] public int MaxPoints { get; set; } = 10000;
+	[Property] public int TracesPerFrame { get; set; } = 500;
+	[Property] public TraceTypes TraceType { get; set; } = TraceTypes.Ray;
+
+	public record struct Hitpoint( Vector3 Position, Vector3 Normal );
+
+	public enum TraceTypes
+	{
+		Ray,
+		Box,
+		Sphere
+	}
+
+
+	List<Hitpoint> worldPoints = new();
+
+	public override void Update()
+	{
+		for ( int i = 0; i < TracesPerFrame; i++ )
+		{
+			PhysicsTraceResult t = default;
+			var start = GameObject.WorldTransform.Position + GameObject.WorldTransform.Forward * 50.0f;
+			var end = GameObject.WorldTransform.Position + GameObject.WorldTransform.Forward * 1000 + Vector3.Random * 400;
+
+			if ( TraceType == TraceTypes.Ray )
+			{
+				t = Physics.Trace
+						.Ray( start, end )
+						.Run();
+			}
+			else if (  TraceType == TraceTypes.Box )
+			{
+				t = Physics.Trace
+						.Ray( start, end )
+						.Size( new BBox( -10, 10 ) )
+						.Run();
+			}
+			else if ( TraceType == TraceTypes.Sphere )
+			{
+				t = Physics.Trace
+						.Ray( start, end )
+						.Radius( 20 )
+						.Run();
+			}
+
+			if ( t.Hit )
+			{
+				worldPoints.Add( new Hitpoint { Position = t.HitPosition, Normal = t.Normal } );
+			}
+
+
+		}
+
+		foreach ( var t in worldPoints )
+		{
+			Gizmo.Draw.Color = new Color( (t.Normal.x + 1) * 0.5f, (t.Normal.y + 1) * 0.5f, (t.Normal.z + 1) * 0.5f );
+			Gizmo.Draw.Line( t.Position, t.Position + t.Normal * 3.0f );
+		}
+
+
+		while ( worldPoints.Count > MaxPoints )
+		{
+			worldPoints.RemoveAt( 0 );
+		}
+	}
+}
