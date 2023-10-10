3eab03853fd70f82347cf1f24d69f523da49421c
Garry Newman <garrynewman@users.noreply.github.com>
2023-10-10 13:42:37 +0100

    Re-save scenes with package information

--- code/GameEngine/SceneUtility.cs
+++ code/GameEngine/SceneUtility.cs
@@ -20,7 +20,7 @@ public static class SceneUtility
 		// Find all guids with "Id" as their name. Add them to translate 
 		// with a new target value.
 		//
-		WalkJsonValues( json, ( k, v ) =>
+		Sandbox.Json.WalkJsonTree( json, ( k, v ) =>
 		{
 			if ( k != "Id" ) return v;
 
@@ -36,7 +36,7 @@ public static class SceneUtility
 		// Find every guid and translate them, but only if they're in our
 		// guid dictionary.
 		//
-		WalkJsonValues( json, ( k, v ) =>
+		Sandbox.Json.WalkJsonTree( json, ( k, v ) =>
 		{
 			if ( !v.TryGetValue<Guid>( out var guid ) ) return v;
 			if ( !translate.TryGetValue( guid, out var updatedGuid ) ) return v;
@@ -45,41 +45,6 @@ public static class SceneUtility
 		} );
 	}
 
-	static JsonNode WalkJsonValues( JsonNode node, Func<string, JsonValue, JsonNode> onValue, string keyName = null )
-	{
-		Action deferred = default;
-
-		if ( node is JsonObject jsonObject )
-		{
-			foreach ( var entry in jsonObject )
-			{
-				var key = entry.Key;
-				var newValue = WalkJsonValues( entry.Value, onValue, key );
-				if ( newValue == entry.Value ) continue;
-
-				deferred += () =>
-				{
-					jsonObject[key] = newValue;
-				};
-			}
-		}
-
-		if ( node is JsonArray array )
-		{
-			foreach ( var a in array )
-			{
-				WalkJsonValues( a, onValue );
-			}
-		}
-
-		if ( node is JsonValue value )
-		{
-			return onValue( keyName, value );
-		}
-
-		deferred?.Invoke();
-		return node;
-	}
 
 	/// <summary>
 	/// Create a unique copy of the passed in GameObject
