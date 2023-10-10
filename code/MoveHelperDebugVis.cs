5e11945c0a93b2d09cad27ab46dad32665374dc6
Garry Newman <garrynewman@users.noreply.github.com>
2023-10-10 10:49:49 +0100

    Debug components for Trace and CharacterControllerHelper

---
+++ code/MoveHelperDebugVis.cs
@@ -0,0 +1,45 @@
+using Sandbox;
+
+public sealed class MoveHelperDebugVis : BaseComponent
+{
+	[Property] Vector3 Gravity { get; set; }
+	[Property] Vector3 Velocity { get; set; }
+
+	public override void Update()
+	{
+		
+	}
+
+
+	public override void DrawGizmos()
+	{
+		var ts = (1.0f / 15.0f);
+
+		BBox box = new BBox( new Vector3( -8, -8, 0 ), new Vector3( 8, 8, 48 ) );
+		var pos = GameObject.WorldTransform.Position;
+		var velocity = GameObject.WorldTransform.Rotation * Velocity;
+
+		for ( int i=0; i<100; i++ )
+		{
+			CharacterControllerHelper move = new CharacterControllerHelper( Scene.PhysicsWorld, pos, velocity );
+			move.Trace = move.Trace.Size( box );
+			//move.TryMoveWithStep( ts, 20.0f );
+			move.TryMove( ts );
+
+			pos = move.Position;
+			velocity = move.Velocity;
+
+			velocity = velocity + Gravity;
+			velocity = velocity + GameObject.WorldTransform.Rotation * Velocity * 0.1f;
+
+			using ( Gizmo.Scope( $"box {i}" ) )
+			{
+				Gizmo.Transform = new Transform( pos );
+				Gizmo.Draw.Color = Color.White.WithAlpha( 0.8f );
+				Gizmo.Draw.LineBBox( box );
+			}
+		}
+
+		
+	}
+}
---
+++ code/TraceDebugVis.cs
@@ -0,0 +1,79 @@
+using Sandbox;
+using System.Threading;
+
+public sealed class TraceDebugVis : BaseComponent
+{
+	public enum TraceType
+	{
+		Ray,
+		Box,
+		Sphere
+	}
+
+	[Property] public TraceType Type { get; set; }
+	[Property] public Vector3 BoxSize { get; set; } = 3;
+	[Property] public float SphereRadius { get; set; } = 2;
+	[Property] public float TraceLength { get; set; } = 100;
+
+
+
+	public override void DrawGizmos()
+	{
+		var bb = new BBox( -BoxSize, BoxSize );
+
+		Gizmo.Transform = Transform.Zero;
+
+		var pos = GameObject.WorldTransform.Position;
+		var rot = GameObject.WorldTransform.Rotation;
+
+		var tr = Scene.PhysicsWorld.Trace.Ray( new Ray( pos, rot.Forward ), TraceLength );
+
+		if ( Type == TraceType.Ray )
+		{
+			
+		}
+
+		if ( Type == TraceType.Box )
+		{
+			tr = tr.Size( bb );
+		}
+
+		if ( Type == TraceType.Sphere )
+		{
+			tr = tr.Radius( SphereRadius );
+		}
+
+		var r = tr.Run();
+
+		if ( r.Body is not null )
+		{
+			Gizmo.Draw.Color = Color.Cyan;
+			var closest = r.Body.FindClosestPoint( r.EndPosition );
+			Gizmo.Draw.Line( closest, r.EndPosition );
+
+			Gizmo.Draw.Text( $"{r.Shape}", new Transform( closest + Vector3.Down * 1 ) );
+		}
+
+		if ( r.Shape is not null )
+		{
+			
+		}
+
+		Gizmo.Draw.Color = Color.White;
+		Gizmo.Draw.Line( r.StartPosition, r.EndPosition );
+		
+
+		if ( Type == TraceType.Box )
+		{
+			Gizmo.Draw.LineBBox( bb + r.StartPosition );
+			Gizmo.Draw.LineBBox( bb + r.EndPosition );
+		}
+
+		Gizmo.Draw.Color = Color.Green;
+		Gizmo.Draw.Line( r.EndPosition, r.EndPosition + r.Normal * 2.0f );
+
+		Gizmo.Draw.Color = Color.White;
+		Gizmo.Draw.Text( $"Normal: {r.Normal}\nFraction: {r.Fraction}", new Transform( r.EndPosition + Vector3.Down * 1 ) );
+
+	}
+}
