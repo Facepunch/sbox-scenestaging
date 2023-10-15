1f6dea3fd4b8021841c3d7c972fa4a9b516066a7
Tony Ferguson <devultj@gmail.com>
2023-10-15 13:38:10 +0100

    Add icon for sound listener

--- code/GameEngine/Components/Audio/SoundListenerComponent.cs
+++ code/GameEngine/Components/Audio/SoundListenerComponent.cs
@@ -3,6 +3,7 @@ using Sandbox;
 [Category( "Audio" )]
 [Title( "Sound Listener" )]
 [Icon( "volume_down", "red", "white" )]
+[EditorHandle( "materials/gizmo/audiolistener.png" )]
 public sealed class SoundListenerComponent : BaseComponent
 {
 	public override void Update()
