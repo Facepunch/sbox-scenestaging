420113d96be8c6c983de8ba6e91105727f8a91bd
James King <james.king@facepunchstudios.com>
2023-10-26 18:50:34 +0200

    ActionGraphComponent

---
+++ code/GameEngine/Components/ActionGraph/ActionGraphComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Sandbox;
+
+[Title( "Action Graph Component" )]
+[Category( "Physics" )]
+[Icon( "account_tree", "red", "white" )]
+public class ActionGraphComponent : BaseComponent
+{
+	public delegate Task GameObjectHandler( GameObject self );
+
+	[Title( "Start" ), Property] public GameObjectHandler HandleStart { get; set; }
+	[Title( "Update" ), Property] public GameObjectHandler HandleUpdate { get; set; }
+	[Title( "Destroy" ), Property] public GameObjectHandler HandleDestroy { get; set; }
+
+	public override void OnStart()
+	{
+		HandleStart?.Invoke( GameObject );
+	}
+
+	public override void Update()
+	{
+		HandleUpdate?.Invoke( GameObject );
+	}
+
+	public override void OnDestroy()
+	{
+		HandleDestroy?.Invoke( GameObject );
+	}
+}
