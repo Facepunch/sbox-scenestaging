7c080667ffb5ceaa03d27187fa61b5cbe54c2d66
Garry Newman <garrynewman@users.noreply.github.com>
2023-10-28 09:51:39 +0100

    Fix exception whe ncopying

--- code/Editor/SceneEditorMenus.cs
+++ code/Editor/SceneEditorMenus.cs
@@ -29,12 +29,13 @@ public static class SceneEditorMenus
 	public static void Cut()
 	{
 		var options = new GameObject.SerializeOptions();
-		var go = EditorScene.Selection.First() as GameObject;
+		var go = EditorScene.Selection.FirstOrDefault() as GameObject;
+		if ( go is null ) return;
+
 		var json = go.Serialize( options );
 
 		EditorUtility.Clipboard.Copy( json.ToString() );
 		go.Destroy();
-
 	}
 
 
@@ -42,7 +43,9 @@ public static class SceneEditorMenus
 	public static void Copy()
 	{
 		var options = new GameObject.SerializeOptions();
-		var go = EditorScene.Selection.First() as GameObject;
+		var go = EditorScene.Selection.FirstOrDefault() as GameObject;
+		if ( go is null ) return;
+
 		var json = go.Serialize( options );
 
 		EditorUtility.Clipboard.Copy( json.ToString() );
