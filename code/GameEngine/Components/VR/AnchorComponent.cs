d22bca4fb436cc8204b50c923f66def7504b9561
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2023-10-31 18:27:07 +0000

    Squash commit of VR components from vr branch

---
+++ code/GameEngine/Components/VR/AnchorComponent.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+
+[Title( "Anchor" )]
+[Category( "VR" )]
+[Icon( "anchor" )]
+public class AnchorComponent : BaseComponent
+{
+	[Flags]
+	public enum UpdateTypes
+	{
+		None,
+		OnPreRender,
+		Update,
+
+		All = OnPreRender | Update
+	}
+
+	[Property]
+	public UpdateTypes UpdateType { get; set; }
+
+	/// <summary>
+	/// Update <see cref="VR.Anchor"/> based on the gameobject's transform
+	/// </summary>
+	private void UpdateAnchor()
+	{
+		VR.Anchor = GameObject.Transform.World;
+	}
+
+	public override void Update()
+	{
+		if ( !Enabled || Scene.IsEditor )
+			return;
+
+		if ( UpdateType.HasFlag( UpdateTypes.Update ) )
+		{
+			UpdateAnchor();
+		}
+	}
+
+	protected override void OnPreRender()
+	{
+		if ( !Enabled || Scene.IsEditor )
+			return;
+
+		if ( UpdateType.HasFlag( UpdateTypes.OnPreRender ) )
+		{
+			UpdateAnchor();
+		}
+	}
+}
