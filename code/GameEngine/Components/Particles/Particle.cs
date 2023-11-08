ff6ffc1aafcb7ca027338fe51002cac1286c2e3a
Garry Newman <garrynewman@users.noreply.github.com>
2023-11-08 18:30:48 +0000

    Particle pool

--- code/GameEngine/Components/Particles/Particle.cs
+++ code/GameEngine/Components/Particles/Particle.cs
@@ -1,4 +1,6 @@
-﻿namespace Sandbox;
+﻿using System.Collections.Generic;
+
+namespace Sandbox;
 
 public class Particle
 {
@@ -11,4 +13,20 @@ public class Particle
 	public float Radius;
 	public Angles Angles;
 	public int Frame;
+
+	public static Queue<Particle> Pool = new ( 512 );
+
+	public static Particle Create()
+	{
+		if ( !Pool.TryDequeue( out Particle p ) )
+		{
+			p = new Particle();
+		}
+
+		p.BornTime = Time.Now;
+		p.Frame = 0;
+		p.Velocity = 0;
+
+		return p;
+	}
 }
