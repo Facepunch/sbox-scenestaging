6b05fdf7a33a352d781a014bd102aead552cba22
Garry Newman <garrynewman@users.noreply.github.com>
2023-11-08 13:21:03 +0000

    Box emitter

---
+++ code/GameEngine/Components/Particles/Emitter/ParticleEmitter.cs
@@ -0,0 +1,29 @@
+﻿namespace Sandbox;
+
+public abstract class ParticleEmitter : BaseComponent, BaseComponent.ExecuteInEditor
+{
+	[Property, Range( 0, 1000 )] public float Initial { get; set; } = 10.0f;
+	[Property, Range( 0, 1000 )] public float Rate { get; set; } = 1.0f;
+
+	public float time;
+
+	public override void OnEnabled()
+	{
+		time = Initial;
+	}
+
+	public override void Update()
+	{
+		if ( !TryGetComponent( out ParticleEffect effect ) ) return;
+
+		time += Time.Delta * Rate;
+
+		while ( !effect.IsFull && time >= 1.0f )
+		{
+			Emit( effect );
+			time -= 1.0f;
+		}
+	}
+
+	public abstract bool Emit( ParticleEffect target );
+}
---
+++ code/GameEngine/Components/Particles/Emitter/ParticleSphereEmitter.cs
@@ -0,0 +1,53 @@
+﻿using Editor;
+using Sandbox.Utility;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Serialization;
+
+namespace Sandbox.GameEngine.Components.Particles.Emitter;
+
+public sealed class ParticleSphereEmitter : ParticleEmitter
+{
+	[Property, Range( 0, 100 )] public float Radius { get; set; } = 20.0f;
+	[Property, Range( -1000, 1000 )] public float Velocity { get; set; } = 100.0f;
+	[Property] public bool OnEdge { get; set; } = false;
+
+
+	public override void DrawGizmos()
+	{
+		if ( !Gizmo.IsSelected )
+			return;
+
+		Gizmo.Draw.Color = Color.White.WithAlpha( 0.1f );
+		Gizmo.Draw.LineSphere( 0, Radius );
+
+		// TODO - Sphere Gizmo
+		
+	}
+
+	public override bool Emit( ParticleEffect target )
+	{
+		var random = Vector3.Random;
+		var offset = random;
+		var radius = Radius * Transform.Scale;
+		var pos = Transform.Position;
+
+		if ( OnEdge )
+		{
+			pos += random.Normal * radius;
+		}
+		else
+		{
+			pos += random * radius;
+		}
+
+		var p = target.Emit( pos );
+
+		if ( Velocity != 0.0f )
+		{
+			p.Velocity += offset * Velocity;
+		}
+
+		return true;
+	}
+}
