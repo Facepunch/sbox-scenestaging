6d47f17dc216340adf4c880efaeeb92e5c24a3de
Garry Newman <garrynewman@users.noreply.github.com>
2023-11-09 09:36:22 +0000

    Create ParticleConeEmitter.cs

---
+++ code/GameEngine/Components/Particles/Emitter/ParticleConeEmitter.cs
@@ -0,0 +1,66 @@
+﻿using Editor;
+using Sandbox.Utility;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Serialization;
+using static Sandbox.Gizmo;
+
+namespace Sandbox.GameEngine.Components.Particles.Emitter;
+
+public sealed class ParticleConeEmitter : ParticleEmitter
+{
+	[Property] public Vector3 Size { get; set; } = 50.0f;
+	[Property, Range( 0, 45 )] public float ConeAngle { get; set; } = 30.0f;
+	[Property] public float ConeNear { get; set; } = 1.0f;
+	[Property] public float ConeFar { get; set; } = 50.0f;
+	[Property] public bool OnEdge { get; set; } = false;
+	[Property] public bool InVolume { get; set; } = false;
+	[Property, Range( 0, 10 )] public float Velocity { get; set; } = 1;
+
+	public override void DrawGizmos()
+	{
+		if ( !Gizmo.IsSelected )
+			return;
+
+		Gizmo.Draw.Color = Color.White.WithAlpha( 0.3f );
+		Gizmo.Draw.LineCircle( Vector3.Forward * (ConeFar - ConeNear), ConeFar * MathF.Tan( ConeAngle.DegreeToRadian() ) );
+		Gizmo.Draw.LineCircle( Vector3.Forward * 0, ConeNear * MathF.Tan( ConeAngle.DegreeToRadian() ) );
+
+		// TODO - Box Resize Gizmo
+		
+	}
+
+	public override bool Emit( ParticleEffect target )
+	{
+		var len = Random.Shared.Float( ConeNear, ConeFar );
+		var radius = len * MathF.Tan( ConeAngle.DegreeToRadian() );
+
+		if ( !OnEdge )
+		{
+			radius = Random.Shared.Float( 0, radius );
+		}
+
+		var pos = Vector3.Forward * 0.1f + Vector3.Forward * (len - ConeNear);
+
+		var angle = Random.Shared.Float( 0, MathF.PI * 2.0f );
+		pos += Vector3.Left * MathF.Sin( angle ) * radius;
+		pos += Vector3.Up * MathF.Cos( angle ) * radius;
+
+		var emitPos = pos;
+
+		if ( !InVolume && !OnEdge )
+		{
+			emitPos = 0;
+		}
+
+		var p = target.Emit( Transform.World.PointToWorld( emitPos ) );
+
+		p.Velocity = Transform.World.NormalToWorld( pos.Normal ) * p.Velocity.Length;
+
+		
+
+
+
+		return true;
+	}
+}
