44c3d25c9b01eb6578e75c9fe3dbb9bcce778e44
Tony Ferguson <devultj@gmail.com>
2023-11-14 18:05:56 +0000

    Add some shorthand methods for PanelComponent

---
+++ code/GameEngine/Components/UI/PanelComponent.Accessors.cs
@@ -0,0 +1,17 @@
+using Sandbox.Razor;
+using Sandbox.UI;
+
+public partial class PanelComponent 
+{
+	/// <inheritdoc cref="Panel.HasClass(string)"/>
+	public bool HasClass( string className ) => Panel.HasClass( className );
+
+	/// <inheritdoc cref="Panel.RemoveClass(string)"/>
+	public void RemoveClass( string className ) => Panel.RemoveClass( className );
+
+	/// <inheritdoc cref="Panel.AddClass(string)"/>
+	public void AddClass( string className ) => Panel.AddClass( className );
+
+	/// <inheritdoc cref="Panel.BindClass(string, Func{bool})"/>
+	public void BindClass( string className, Func<bool> func ) => Panel.BindClass( className, func );
+}
