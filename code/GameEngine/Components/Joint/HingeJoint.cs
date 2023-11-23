1cd834447859e66e7ce1d8013e7d0ae1afee1b02
Layla <fortblox@gmail.com>
2023-11-23 14:47:06 +0000

    Hinge limits and friction

--- code/GameEngine/Components/Joint/HingeJoint.cs
+++ code/GameEngine/Components/Joint/HingeJoint.cs
@@ -9,9 +9,55 @@ public sealed class HingeJoint : Joint
 	[Property] public Vector3 Center { get; set; }
 	[Property] public Vector3 Axis { get; set; } = Vector3.Forward;
 
+	[Property] 
+	public float MaxAngle
+	{
+		get => hingeJoint.IsValid() ? hingeJoint.MaxAngle : default;
+		set
+		{
+			if ( hingeJoint.IsValid() )
+			{
+				hingeJoint.MaxAngle = value;
+			}
+		}
+	}
+
+	[Property]
+	public float MinAngle
+	{
+		get => hingeJoint.IsValid() ? hingeJoint.MinAngle : default;
+		set
+		{
+			if ( hingeJoint.IsValid() )
+			{
+				hingeJoint.MinAngle = value;
+			}
+		}
+	}
+
+	private float friction;
+
+	[Property]
+	public float Friction
+	{
+		get => friction;
+		set
+		{
+			friction = value;
+
+			if ( hingeJoint.IsValid() )
+			{
+				hingeJoint.Friction = value;
+			}
+		}
+	}
+
+	private Sandbox.Physics.HingeJoint hingeJoint;
+
 	protected override PhysicsJoint CreateJoint( PhysicsBody body1, PhysicsBody body2 )
 	{
-		return PhysicsJoint.CreateHinge( body1, body2, body1.Transform.PointToWorld( Center ), Axis );
+		hingeJoint = PhysicsJoint.CreateHinge( body1, body2, body1.Transform.PointToWorld( Center ), Axis );
+		return hingeJoint;
 	}
 
 	public override void DrawGizmos()
