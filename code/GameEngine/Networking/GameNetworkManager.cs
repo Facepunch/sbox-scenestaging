b7e31d0596083c473a5a5abd44fc6bef14e39689
Garry Newman <garrynewman@users.noreply.github.com>
2023-11-24 16:01:32 +0000

    Host creates the player object, rather than having player create their own

--- code/GameEngine/Networking/GameNetworkManager.cs
+++ code/GameEngine/Networking/GameNetworkManager.cs
@@ -1,26 +1,32 @@
 using Sandbox;
 
-public sealed class GameNetworkManager : BaseComponent
+public sealed class GameNetworkManager : BaseComponent, BaseComponent.INetworkListener
 {
 	[Property] public GameObject PlayerPrefab { get; set; }
 	[Property] public GameObject SpawnPoint { get; set; }
 
 	public override void OnStart()
 	{
-		var myPlayerObject = SceneUtility.Instantiate( PlayerPrefab, SpawnPoint.Transform.World );
-		myPlayerObject.Enabled = true;
 
-		var nameTag = myPlayerObject.GetComponent<NameTagPanel>( false, true );
-		if ( nameTag is not null )
-		{
-			nameTag.Name = Game.UserName;
-		}
-
-		myPlayerObject.Network.Spawn();
 	}
 
 	public override void Update()
 	{
 		
+	}
+
+	public void OnActive( NetworkChannel channel )
+	{
+		Log.Info( $"Player '{channel.DisplayName}' is becoming active" );
+
+		var player = SceneUtility.Instantiate( PlayerPrefab, SpawnPoint.Transform.World );
+
+		var nameTag = player.GetComponent<NameTagPanel>( false, true );
+		if ( nameTag is not null )
+		{
+			nameTag.Name = channel.DisplayName;
+		}
+
+		player.Network.Spawn( channel );
 	}
 }
