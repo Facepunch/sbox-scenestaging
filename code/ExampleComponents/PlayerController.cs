c56094659e361962c7dc1588d578690a4411f051
bakscratch <lt_8@hotmail.co.uk>
2023-12-02 13:26:34 +0000

    Add AnimationHelper.WithWishVelocity to example playercontroller

--- code/ExampleComponents/PlayerController.cs
+++ code/ExampleComponents/PlayerController.cs
@@ -62,6 +62,7 @@ public class PlayerController : BaseComponent, INetworkSerializable
 		if ( AnimationHelper is not null )
 		{
 			AnimationHelper.WithVelocity( cc.Velocity );
+			AnimationHelper.WithWishVelocity( WishVelocity );
 			AnimationHelper.IsGrounded = cc.IsOnGround;
 			AnimationHelper.FootShuffle = rotateDifference;
 			AnimationHelper.WithLook( EyeAngles.Forward, 1, 1, 1.0f );
