ae268f0acb7f14679623e8ca8e22e3190313373d
James King <james.king@facepunchstudios.com>
2023-12-07 23:14:31 +0000

    Actions example

---
+++ code/ExampleComponents/UpdateAction.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+
+public class UpdateAction : Component
+{
+	public delegate Task UpdateFunc( GameObject self, float dt );
+
+	[Property]
+	public UpdateFunc Update { get; set; }
+
+	protected override void OnUpdate()
+	{
+		_ = Update?.Invoke( GameObject, Time.Delta );
+	}
+}
