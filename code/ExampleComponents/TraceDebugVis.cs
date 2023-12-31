68b96c21decdfa8434f87546ca7b7d90496c7e0e
Garry Newman <garrynewman@users.noreply.github.com>
2023-12-31 11:07:39 +0000

    Add trace test scene

--- code/ExampleComponents/TraceDebugVis.cs
+++ code/ExampleComponents/TraceDebugVis.cs
@@ -14,8 +14,14 @@ public sealed class TraceDebugVis : Component
 	[Property] public Vector3 BoxSize { get; set; } = 3;
 	[Property] public float SphereRadius { get; set; } = 2;
 	[Property] public float TraceLength { get; set; } = 100;
+	[Property] public List<GameObject> IgnoreSingleObjects { get; set; }
+	[Property] public List<GameObject> IgnoreHierarchy { get; set; }
+	[Property] public TagSet IgnoreTags { get; set; }
 
-
+	protected override void OnUpdate()
+	{
+		DrawGizmos();
+	}
 
 	protected override void DrawGizmos()
 	{
@@ -26,7 +32,7 @@ public sealed class TraceDebugVis : Component
 		var pos = Transform.Position;
 		var rot = Transform.Rotation;
 
-		var tr = Scene.PhysicsWorld.Trace.Ray( new Ray( pos, rot.Forward ), TraceLength );
+		var tr = Scene.Trace.Ray( new Ray( pos, rot.Forward ), TraceLength );
 
 		if ( Type == TraceType.Ray )
 		{
@@ -43,15 +49,33 @@ public sealed class TraceDebugVis : Component
 			tr = tr.Radius( SphereRadius );
 		}
 
+		if ( IgnoreSingleObjects is not null )
+		{
+			foreach( var obj in IgnoreSingleObjects )
+			{
+				tr = tr.IgnoreGameObject( obj );
+			}
+		}
+
+		if ( IgnoreHierarchy is not null )
+		{
+			foreach ( var obj in IgnoreHierarchy )
+			{
+				tr = tr.IgnoreGameObjectHierarchy( obj );
+			}
+		}
+
+		if ( IgnoreTags  is not null )
+		{
+			tr = tr.WithoutTags( IgnoreTags );
+		}
+
 		var r = tr.Run();
 
-		if ( r.Body is not null )
+		if ( r.GameObject is not null )
 		{
-			Gizmo.Draw.Color = Color.Cyan;
-			var closest = r.Body.FindClosestPoint( r.EndPosition );
-			Gizmo.Draw.Line( closest, r.EndPosition );
-
-			Gizmo.Draw.Text( $"{r.Shape}", new Transform( closest + Vector3.Down * 1 ) );
+			Gizmo.Draw.Color = Color.White;
+			Gizmo.Draw.Text( $"{r.GameObject?.Name}", new Transform( r.HitPosition + Vector3.Down * 7 ), "Poppins", 18 );
 		}
 
 		if ( r.Shape is not null )
@@ -59,7 +83,8 @@ public sealed class TraceDebugVis : Component
 			
 		}
 
-		Gizmo.Draw.Color = Color.White;
+		Gizmo.Draw.Color = Color.White.WithAlpha( 0.1f );
+		Gizmo.Draw.LineThickness = 4;
 		Gizmo.Draw.Line( r.StartPosition, r.EndPosition );
 		
 
@@ -72,8 +97,8 @@ public sealed class TraceDebugVis : Component
 		Gizmo.Draw.Color = Color.Green;
 		Gizmo.Draw.Line( r.EndPosition, r.EndPosition + r.Normal * 2.0f );
 
-		Gizmo.Draw.Color = Color.White;
-		Gizmo.Draw.Text( $"Normal: {r.Normal}\nFraction: {r.Fraction}", new Transform( r.EndPosition + Vector3.Down * 1 ) );
+		//Gizmo.Draw.Color = Color.White;
+		//Gizmo.Draw.Text( $"Normal: {r.Normal}\nFraction: {r.Fraction}", new Transform( r.EndPosition + Vector3.Down * 1 ) );
 
 	}
 }
