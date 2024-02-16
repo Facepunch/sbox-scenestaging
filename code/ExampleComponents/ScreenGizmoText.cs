095980027588c77110989d256e099c1f5cb4f630
cr4yz <jacob.ee92@gmail.com>
2024-02-16 14:44:24 -0600

    Add text with tabs test

---
+++ code/ExampleComponents/ScreenGizmoText.cs
@@ -0,0 +1,12 @@
+﻿
+internal class ScreenGizmoText : Component
+{
+
+	protected override void DrawGizmos()
+	{
+		base.DrawGizmos();
+
+		Gizmo.Draw.ScreenText( "Tab test:\t1\t\t\tColumn\nTab test:\t2\t\t\tColumn", new( 64, 64 ), size: 20, flags: TextFlag.LeftCenter );
+	}
+
+}
