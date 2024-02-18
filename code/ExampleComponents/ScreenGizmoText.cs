511956f6124c1c093a6a44f8d24ec3c00022caa6
cr4yz <jacob.ee92@gmail.com>
2024-02-17 19:35:47 -0600

    Update tab test

--- code/ExampleComponents/ScreenGizmoText.cs
+++ code/ExampleComponents/ScreenGizmoText.cs
@@ -6,7 +6,8 @@ internal class ScreenGizmoText : Component
 	{
 		base.DrawGizmos();
 
-		Gizmo.Draw.ScreenText( "Tab test:\t1\t\t\tColumn\nTab test:\t2\t\t\tColumn", new( 64, 64 ), size: 20, flags: TextFlag.LeftCenter );
+		Gizmo.Draw.ScreenText( "Tab testt:\t1\t\tColumn\nTab test:\t2", new( 64, 64 ), size: 20, flags: TextFlag.LeftCenter );
+		Gizmo.Draw.ScreenText( "Tab   test:\t3\t\tColumn", new( 64, 110 ), size: 20, flags: TextFlag.LeftCenter );
 	}
 
 }
