dc400e19b0ebf00f185e6876f249636996a6b9fc
Garry Newman <garrynewman@users.noreply.github.com>
2024-05-20 14:57:16 +0100

    Swallow EscapePressed when returning to menu

--- code/ExampleComponents/SceneMenu/ReturnToMenu.cs
+++ code/ExampleComponents/SceneMenu/ReturnToMenu.cs
@@ -1,11 +1,10 @@
-using Sandbox;
-
 public sealed class ReturnToMenu : Component
 {
 	protected override void OnUpdate()
 	{
 		if ( Input.EscapePressed )
 		{
+			Input.EscapePressed = false;
 			Game.ActiveScene.LoadFromFile( "scenes/tests/menu.scene" );
 		}
 	}
