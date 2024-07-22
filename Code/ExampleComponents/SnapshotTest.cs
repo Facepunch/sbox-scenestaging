73dfeac584309287ba4b82ee5a838d1f191a6d63
James King <james.king@facepunchstudios.com>
2024-07-22 11:14:58 +0100

    Add RPC spam scene

---
+++ Code/ExampleComponents/SnapshotTest.cs
@@ -0,0 +1,30 @@
+using Sandbox;
+
+public sealed class SnapshotTest : Component
+{
+	[Property]
+	public int Counter { get; set; }
+
+	[Property]
+	public TextRenderer Text { get; set; }
+
+	protected override void OnUpdate()
+	{
+		if ( Networking.IsHost )
+		{
+			Count( Counter + 1 );
+		}
+	}
+
+	[Broadcast( NetPermission.HostOnly )]
+	public void Count( int value )
+	{
+		if ( Counter != value - 1 )
+		{
+			Log.Warning( $"Jumping from {Counter} to {value - 1}!" );
+		}
+
+		Counter = value;
+		Text.Text = value.ToString();
+	}
+}
