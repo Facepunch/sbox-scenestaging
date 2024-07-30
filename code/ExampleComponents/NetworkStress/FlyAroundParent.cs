95e89c3349572e5801f848481a8b1a7aef5e9d01
kurozael <kurozael@gmail.com>
2024-07-30 15:33:17 +0100

    Player move / locally owned network object parented orbiters

---
+++ code/ExampleComponents/NetworkStress/FlyAroundParent.cs
@@ -0,0 +1,28 @@
+﻿public class FlyAroundParent : Component
+{
+	private Vector3 Direction { get; set; }
+	
+	protected override void OnStart()
+	{
+		if ( !IsProxy )
+		{
+			Direction = Vector3.Random;
+		}
+		
+		base.OnStart();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if ( !IsProxy )
+		{
+			var localPos = Transform.LocalPosition;
+			localPos.x = Direction.x * MathF.Sin( Time.Now ) * 64f;
+			localPos.y = Direction.y * MathF.Cos( Time.Now ) * 64f;
+			localPos.z = 80f + MathF.Sin( Time.Now ) * 8f + MathF.Cos( Time.Now ) * 8f;
+			Transform.LocalPosition = localPos;
+		}
+		
+		base.OnFixedUpdate();
+	}
+}
---
+++ code/ExampleComponents/NetworkStress/SpawnNetworkedObjects.cs
@@ -0,0 +1,24 @@
+﻿public class SpawnNetworkedObjects : Component
+{
+	[Property] public GameObject PrefabToSpawn { get; set; }
+	[Property] public bool ParentToThis { get; set; }
+	[Property] public int AmountToSpawn { get; set; } = 3;
+	
+	protected override void OnStart()
+	{
+		if ( !IsProxy )
+		{
+			for ( var i = 0; i < AmountToSpawn; i++ )
+			{
+				var go = PrefabToSpawn.Clone( Transform.Position, Rotation.Identity );
+
+				if ( ParentToThis )
+					go.SetParent( GameObject );
+			
+				go.NetworkSpawn();
+			}
+		}
+		
+		base.OnStart();
+	}
+}
