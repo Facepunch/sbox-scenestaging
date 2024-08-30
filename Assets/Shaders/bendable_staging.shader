32aebc618531d51c1b11ee486148ecf8784daf52
Sam Pavlovic <sam@sampavlovic.com>
2024-08-29 22:49:40 -0300

    WIP Lattice Deform https://files.facepunch.com/sam/1b2911b1/sbox-dev_5LFnSp7Xlr.mp4

--- Assets/Shaders/bendable_staging.shader
+++ Assets/Shaders/bendable_staging.shader
@@ -1,6 +1,13 @@
 HEADER
 {
-	Description = "Template Shader for S&box";
+	Description = "Standard shader but it bends, proof of concept, will be made into a proper feature with compute shaders";
+}
+
+MODES
+{
+	VrForward();
+	Depth();
+	ToolsVis( S_MODE_TOOLS_VIS );
 }
 
 FEATURES
@@ -27,10 +34,50 @@ VS
 {
 	#include "common/vertex.hlsl"
 
+	StructuredBuffer<float4> Lattice < Attribute("Lattice"); >;
+	float4x4 LocalToLattice < Attribute( "LocalToLattice" ); >;
+	float3 Scale < Attribute( "Scale" ); >;
+	float3 Segments < Attribute( "Segments"); >;
+
+	int Convert3DTo1D(int x, int y, int z)
+	{
+		return x * (Segments.y * Segments.z) + y * Segments.z + z;
+	}
+
 	PixelInput MainVs( VertexInput i )
 	{
-		PixelInput o = ProcessVertex( i );
-		// Add your vertex manipulation functions here
+		float3 PositionToLattice = mul( LocalToLattice, float4( i.vPositionOs, 1 ) ).xyz;
+		float3 vPositionOs = i.vPositionOs;
+		// Only influence if bbox is inside the vertex
+		if (all(PositionToLattice >= 0.0f && PositionToLattice <= 1.0f))
+        {
+			// Get all 8 neighbors
+			float3 frac = PositionToLattice * Segments;
+			int3 p = floor(frac);
+
+			float3 neighbors[8];
+			neighbors[0] = Lattice[Convert3DTo1D(p.x, p.y, p.z)].xyz;
+			neighbors[1] = Lattice[Convert3DTo1D(p.x + 1, p.y, p.z)].xyz;
+			neighbors[2] = Lattice[Convert3DTo1D(p.x, p.y + 1, p.z)].xyz;
+			neighbors[3] = Lattice[Convert3DTo1D(p.x + 1, p.y + 1, p.z)].xyz;
+			neighbors[4] = Lattice[Convert3DTo1D(p.x, p.y, p.z + 1)].xyz;
+			neighbors[5] = Lattice[Convert3DTo1D(p.x + 1, p.y, p.z + 1)].xyz;
+			neighbors[6] = Lattice[Convert3DTo1D(p.x, p.y + 1, p.z + 1)].xyz;
+			neighbors[7] = Lattice[Convert3DTo1D(p.x + 1, p.y + 1, p.z + 1)].xyz;
+
+			frac = frac - p;
+
+			// Lerp each fraction of the 8 neighbors in the right direction
+			// Doesn't look fully correct
+			float3 offset = lerp( lerp( lerp( neighbors[0], neighbors[1], frac.x ), lerp( neighbors[2], neighbors[3], frac.x ), frac.y ), lerp( lerp( neighbors[4], neighbors[5], frac.x ), lerp( neighbors[6], neighbors[7], frac.x ), frac.y ), frac.z );
+			
+			i.vPositionOs += offset * Scale;
+
+			// Todo: warp normals too
+        }
+
+
+		PixelInput o = ProcessVertex( i );		
 		return FinalizeVertex( o );
 	}
 }
@@ -40,11 +87,13 @@ VS
 PS
 {
     #include "common/pixel.hlsl"
+	
 
 	float4 MainPs( PixelInput i ) : SV_Target0
 	{
 		Material m = Material::From( i );
-		/* m.Metalness = 1.0f; // Forces the object to be metalic */
+		m.Metalness = 1.0f;
+		m.Roughness = 0.1f;
 		return ShadingModelStandard::Shade( i, m );
 	}
 }
--- Code/ExampleComponents/LatticeDeform.cs
+++ Code/ExampleComponents/LatticeDeform.cs
@@ -4,52 +4,105 @@ namespace Sandbox.ExampleComponents
 {
 	/// <summary>
 	/// Deforms parent
+	/// Proof of concept, will be made into a proper feature with compute shaders
+	/// Make just a sibling component rather than child, right now I prefer using the GameObject gizmo
 	/// </summary>
 	public class LatticeDeform : Component, Component.ExecuteInEditor
 	{
-		[Property, MakeDirty, Range(2,10, 1)] public Vector3Int Segments { get; set; }
+		// This will be hidden from user once we have gizmos per segment
+		[Property, MakeDirty ] public Vector3Int Segments 
+		{ 
+			get => _segments;
+			set
+			{
+				// Clamp it to minimum 2
+				_segments = Vector3Int.Max( value, new Vector3Int(2,2,2) );
 
-		public List<Vector3> Points { get; private set; }
+				// Clear previous points if we change it
+				if( _segments.x * _segments.y * _segments.z != Points.Count )
+					Points = Enumerable.Repeat( Vector4.Zero, _segments.x * _segments.y * _segments.z ).ToList();
+					
+				PointsBuffer = new ComputeBuffer<Vector4>( Points.Count );
+			}
+		}
+		internal Vector3Int _segments;
+
+		// Needs to be a float4-aligned for ComputeBuffer
+		// Dirty doesn't seem to be called when list elements are changed
+		[Property, MakeDirty] public List<Vector4> Points { get; set; }
+
+		ComputeBuffer<Vector4> PointsBuffer;
+
+		public LatticeDeform()
+		{
+			Points = new List<Vector4>(2*2*2);
+			Segments = new Vector3Int(2,2,2);
+		}
 
 		protected override void OnEnabled()
 		{
+			if( Transform == default )
+			{
+				// Get bounds from parent
+				var parent = Components.GetInParent<ModelRenderer>();
+
+				// Make it use the same bounds as the parent
+				Transform.Scale = parent.Bounds.Size;
+				Transform.Position = parent.Bounds.Mins;
+			}
+
 			base.OnEnabled();
-			//OnDirty();
 		}
 
-		protected override void OnUpdate()
+		protected override void OnDisabled()
 		{
-			// Get bounds from parent
+			base.OnDisabled();
+
 			var parent = Components.GetInParent<ModelRenderer>();
 
-			Transform.Scale = parent.Bounds.Size;
-			Transform.Position = parent.Bounds.Mins;
-
-			Points = new List<Vector3>( Segments.x * Segments.y * Segments.z );
-
-			for ( var x = 0; x < Segments.x; x++ )
-			{
-				for ( var y = 0; y < Segments.y; y++ )
-				{
-					for ( var z = 0; z < Segments.z; z++ )
-					{
-						Points.Add( new Vector3( 
-							x / (float)( Segments.x - 1 ), 
-							y / (float)( Segments.y - 1 ), 
-							z / (float)( Segments.z - 1 )  ) 
-						);
-					}
-				}
-			}
+			parent.SceneObject.Attributes.Set( "LocalToLattice", Matrix.Identity );
+			parent.SceneObject.Attributes.Set( "Segments", Vector3Int.Zero );
 		}
+
+		protected override void OnDirty()
+		{
+			base.OnDirty();
+		}
+
 		public int Convert3DTo1D(int x, int y, int z)
 		{
 			return x * (Segments.y * Segments.z) + y * Segments.z + z;
 		}
-		
+
+		protected override void OnPreRender()
+		{
+			base.OnPreRender();
+			
+			if( PointsBuffer is null )
+				return;
+
+			// Needs update only when dirty
+			PointsBuffer.SetData( Points.ToArray() );
+
+			var parent = Components.GetInParent<ModelRenderer>();
+
+			// Matrix.FromTransform is internal, no way to pass Transform directly
+			var matrix = new Matrix();
+			var transform = Transform.Local;
+			
+			matrix = Matrix.CreateScale(transform.Scale) * Matrix.CreateRotation(transform.Rotation) * Matrix.CreateTranslation(transform.Position);
+			
+			parent.SceneObject.Attributes.Set( "Scale", Transform.Scale );
+			parent.SceneObject.Attributes.Set( "LocalToLattice", matrix.Inverted );
+			parent.SceneObject.Attributes.Set( "Lattice", PointsBuffer );
+			parent.SceneObject.Attributes.Set( "Segments", Segments );
+		}
+
 		protected override void DrawGizmos()
         {
             base.DrawGizmos();
+
+			// Draw Grid
             for( var x = 0; x < Segments.x; x++ )
             {
                 for( var y = 0; y < Segments.y; y++ )
@@ -59,22 +112,31 @@ namespace Sandbox.ExampleComponents
                         // Draw a grid
 						var self = Convert3DTo1D(x, y, z);
 						
-						var neighbors = new List<int>();
+						var neighbors = new List<Vector3Int>();
 
-						if (x + 1 < Segments.x) neighbors.Add(Convert3DTo1D(x + 1, y, z));
-						if (y + 1 < Segments.y) neighbors.Add(Convert3DTo1D(x, y + 1, z));
-						if (z + 1 < Segments.z) neighbors.Add(Convert3DTo1D(x, y, z + 1));
+						// Add neighbors, don't add them on edges
+						if (x + 1 < Segments.x) neighbors.Add( new Vector3Int(x + 1, y, z));
+						if (y + 1 < Segments.y) neighbors.Add( new Vector3Int(x, y + 1, z));
+						if (z + 1 < Segments.z) neighbors.Add( new Vector3Int(x, y, z + 1));
 
+						var o1 = new Vector3( 	x / (float)( Segments.x - 1 ), 
+												y / (float)( Segments.y - 1 ), 
+												z / (float)( Segments.z - 1 ) );
 
-						Gizmo.Draw.Color = Gizmo.Colors.Green.WithAlpha(0.25f);
+						Gizmo.Draw.Color = Color.White.WithAlpha(0.25f);
 						Gizmo.Draw.LineThickness = 4;
-						foreach (var neighbor in neighbors)
+
+						foreach (var n in neighbors)
 						{
-							Gizmo.Draw.Line(Points[self], Points[neighbor]);
+							var o2 = new Vector3( 	n.x / (float)( Segments.x - 1 ), 
+													n.y / (float)( Segments.y - 1 ), 
+													n.z / (float)( Segments.z - 1 ) );
+
+							Gizmo.Draw.Line(Points[self] + o1, Points[ Convert3DTo1D( n.x, n.y, n.z) ] + o2 );
 						}
                     }
                 }
             }
-        }
+		}
 	}
 }
