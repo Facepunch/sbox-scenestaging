90348a528344a6d89e813019630b06081facc5de
Garry Newman <garrynewman@users.noreply.github.com>
2024-10-08 17:02:47 +0100

    DebugDrawSystem

---
+++ Code/ExampleComponents/DebugDrawSystem.cs
@@ -0,0 +1,195 @@
+﻿using Sandbox.Diagnostics;
+
+public class DebugDrawSystem : GameObjectSystem<DebugDrawSystem>
+{
+	bool inFixedUpdate;
+
+	class Entry : IConfigurableDebug, IDisposable
+	{
+		public bool CreatedDuringFixed;
+		public bool SingleFrame = true;
+		public float life;
+		public SceneObject sceneObject;
+
+		public void Dispose()
+		{
+			sceneObject?.Delete();
+			sceneObject = default;
+		}
+
+		IConfigurableDebug IConfigurableDebug.WithColor( Color color )
+		{
+			if ( sceneObject is not null )
+			{
+				sceneObject.Attributes.Set( "g_tint", color );
+				sceneObject.ColorTint = color;
+			}
+
+			return this;
+		}
+
+		IConfigurableDebug IConfigurableDebug.WithTime( float time )
+		{
+			life = time;
+			SingleFrame = false;
+			return this;
+		}
+	}
+
+	List<Entry> entries = new List<Entry>();
+
+	public interface IConfigurableDebug
+	{
+		public IConfigurableDebug WithColor( Color color )
+		{
+			return this;
+		}
+
+		public IConfigurableDebug WithTime( float time );
+	}
+
+
+	public DebugDrawSystem( Scene scene ) : base( scene )
+	{
+		Listen( Stage.StartUpdate, -10000, StartUpdate, "BuildDebugOverlays" );
+		Listen( Stage.StartFixedUpdate, -10000, StartFixedUpdate, "BuildDebugOverlays" );
+		Listen( Stage.FinishFixedUpdate, 10000, EndFixedUpdate, "BuildDebugOverlays" );
+	}
+
+	IConfigurableDebug Add( Entry entry )
+	{
+		entries.Add( entry );
+		entry.CreatedDuringFixed = inFixedUpdate;
+		return entry;
+	}
+
+	public IConfigurableDebug AddLine( Vector3 from, Vector3 to )
+	{
+		var entry = new Entry();
+		entry.life = Time.Delta;
+
+		var so = new SceneDynamicObject( Scene.SceneWorld );
+		so.Transform = Transform.Zero;
+		so.Material = Material.Load( "materials/gizmo/line.vmat" );
+		so.Flags.CastShadows = false;
+		so.Init( Graphics.PrimitiveType.Lines );
+
+		so.AddVertex( new Vertex( from, Color32.White ) );
+		so.AddVertex( new Vertex( to, Color32.White ) );
+
+		entry.sceneObject = so;
+
+		return Add( entry );
+	}
+
+	public IConfigurableDebug AddBox( BBox box ) => AddBox( box, Transform.Zero );
+
+	public IConfigurableDebug AddBox( BBox box, Transform transform )
+	{
+		return AddBox( new Vector3[8]
+{
+			new Vector3( box.Mins.x, box.Mins.y, box.Mins.z ),
+			new Vector3( box.Maxs.x, box.Mins.y, box.Mins.z ),
+			new Vector3( box.Maxs.x, box.Maxs.y, box.Mins.z ),
+			new Vector3( box.Mins.x, box.Maxs.y, box.Mins.z ),
+
+			new Vector3( box.Mins.x, box.Mins.y, box.Maxs.z ),
+			new Vector3( box.Maxs.x, box.Mins.y, box.Maxs.z ),
+			new Vector3( box.Maxs.x, box.Maxs.y, box.Maxs.z ),
+			new Vector3( box.Mins.x, box.Maxs.y, box.Maxs.z )
+		}, transform );
+	}
+
+	private IConfigurableDebug AddBox( Span<Vector3> corners, Transform transform )
+	{
+		var entry = new Entry();
+		entry.life = Time.Delta;
+
+		var so = new SceneDynamicObject( Scene.SceneWorld );
+		so.Transform = transform;
+		so.Material = Material.Load( "materials/gizmo/line.vmat" );
+		so.Flags.CastShadows = false;
+		so.Init( Graphics.PrimitiveType.Lines );
+		Assert.AreEqual( 8, corners.Length );
+
+		so.AddVertex( new Vertex( corners[0], Color32.White ) );
+		so.AddVertex( new Vertex( corners[1], Color32.White ) );
+		so.AddVertex( new Vertex( corners[1], Color32.White ) );
+		so.AddVertex( new Vertex( corners[2], Color32.White ) );
+		so.AddVertex( new Vertex( corners[2], Color32.White ) );
+		so.AddVertex( new Vertex( corners[3], Color32.White ) );
+		so.AddVertex( new Vertex( corners[3], Color32.White ) );
+		so.AddVertex( new Vertex( corners[0], Color32.White ) );
+
+
+		so.AddVertex( new Vertex( corners[0], Color32.White ) );
+		so.AddVertex( new Vertex( corners[4], Color32.White ) );
+		so.AddVertex( new Vertex( corners[1], Color32.White ) );
+		so.AddVertex( new Vertex( corners[5], Color32.White ) );
+		so.AddVertex( new Vertex( corners[2], Color32.White ) );
+		so.AddVertex( new Vertex( corners[6], Color32.White ) );
+		so.AddVertex( new Vertex( corners[3], Color32.White ) );
+		so.AddVertex( new Vertex( corners[7], Color32.White ) );
+
+		so.AddVertex( new Vertex( corners[4], Color32.White ) );
+		so.AddVertex( new Vertex( corners[5], Color32.White ) );
+		so.AddVertex( new Vertex( corners[5], Color32.White ) );
+		so.AddVertex( new Vertex( corners[6], Color32.White ) );
+		so.AddVertex( new Vertex( corners[6], Color32.White ) );
+		so.AddVertex( new Vertex( corners[7], Color32.White ) );
+		so.AddVertex( new Vertex( corners[7], Color32.White ) );
+		so.AddVertex( new Vertex( corners[4], Color32.White ) );
+
+		entry.sceneObject = so;
+
+		return Add( entry );
+	}
+
+	void RemoveExpired()
+	{
+		for ( int i = 0; i < entries.Count; i++ )
+		{
+			if ( entries[i].SingleFrame ) continue;
+			entries[i].life -= Time.Delta;
+
+			if ( entries[i].life < 0 )
+			{
+				entries[i].Dispose();
+				entries.RemoveAt( i ); // move to end and remove?
+				i--;
+			}
+		}
+	}
+
+	void RemoveSingleFrame( bool createdDuringFixed )
+	{
+		for ( int i = 0; i < entries.Count; i++ )
+		{
+			if ( !entries[i].SingleFrame ) continue;
+			if ( entries[i].CreatedDuringFixed != createdDuringFixed ) continue;
+
+			entries[i].Dispose();
+			entries.RemoveAt( i );
+			i--;
+		}
+
+		Scene.SceneWorld.DeletePendingObjects();
+	}
+
+	void StartUpdate()
+	{
+		RemoveExpired();
+		RemoveSingleFrame( false );
+	}
+
+	void StartFixedUpdate()
+	{
+		RemoveSingleFrame( true );
+		inFixedUpdate = true;
+	}
+
+	void EndFixedUpdate()
+	{
+		inFixedUpdate = false;
+	}
+}
