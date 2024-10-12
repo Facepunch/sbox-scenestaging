8448854a79fc08166bfd3c66e9cd9f2b97855c76
Layla <fortblox@gmail.com>
2024-10-12 17:37:51 +0100

    Add work in progress ragdoll component that creates child rigid bodies from model physics https://files.facepunch.com/layla/1b1211b1/sbox-dev_CS4vIaEj8o.mp4

---
+++ code/WorkInProgress/Ragdoll.cs
@@ -0,0 +1,212 @@
+namespace Sandbox;
+
+/// <summary>
+/// Work in progress ragdoll component that creates child rigid bodies from model physics.
+/// </summary>
+public sealed class Ragdoll : Component, Component.ExecuteInEditor
+{
+	Model _model;
+
+	[Property]
+	public Model Model
+	{
+		get => _model;
+		set
+		{
+			if ( _model == value )
+				return;
+
+			_model = value;
+			OnModelChanged();
+		}
+	}
+
+	[Property]
+	public SkinnedModelRenderer Renderer { get; set; }
+
+	private record BoneBodyPair( BoneCollection.Bone Bone, GameObject Body );
+	private readonly List<BoneBodyPair> _bodies = new();
+
+	private void OnModelChanged()
+	{
+		if ( !Active )
+			return;
+
+		CreatePhysics();
+	}
+
+	private void CreatePhysics()
+	{
+		DestroyPhysics();
+
+		if ( !Model.IsValid() )
+			return;
+
+		var physics = Model.Physics;
+		if ( physics is null )
+			return;
+
+		if ( physics.Parts.Count == 0 )
+			return;
+
+		foreach ( var part in physics.Parts )
+		{
+			var go = Scene.CreateObject( false );
+			go.Flags = GameObjectFlags.NotSaved;
+			go.Name = part.BoneName;
+			go.LocalTransform = part.Transform;
+			go.Parent = GameObject;
+			go.AddComponent<Rigidbody>();
+
+			foreach ( var sphere in part.Spheres )
+			{
+				var collider = go.AddComponent<SphereCollider>();
+				collider.Center = sphere.Sphere.Center;
+				collider.Radius = sphere.Sphere.Radius;
+			}
+
+			foreach ( var capsule in part.Capsules )
+			{
+				var collider = go.AddComponent<CapsuleCollider>();
+				collider.Start = capsule.Capsule.CenterA;
+				collider.End = capsule.Capsule.CenterB;
+				collider.Radius = capsule.Capsule.Radius;
+			}
+
+			foreach ( var hull in part.Hulls )
+			{
+				var collider = go.AddComponent<HullCollider>();
+				collider.Type = HullCollider.PrimitiveType.Points;
+				collider.Points = hull.GetPoints().ToList();
+			}
+
+			var bone = Model.Bones.AllBones.FirstOrDefault( x => x.Name == part.BoneName ); ;
+			_bodies.Add( new BoneBodyPair( bone, go ) );
+		}
+
+		foreach ( var jointDesc in physics.Joints )
+		{
+			Joint joint = null;
+			var body = _bodies[jointDesc.Body1].Body;
+
+			if ( jointDesc.Type == PhysicsGroupDescription.JointType.Hinge )
+			{
+				var hingeJoint = body.AddComponent<HingeJoint>();
+				hingeJoint.Attachment = HingeJoint.AttachmentMode.LocalFrames;
+				hingeJoint.LocalFrame1 = jointDesc.Frame1;
+				hingeJoint.LocalFrame2 = jointDesc.Frame2;
+
+				if ( jointDesc.EnableTwistLimit )
+				{
+					hingeJoint.MinAngle = jointDesc.TwistMin.RadianToDegree();
+					hingeJoint.MaxAngle = jointDesc.TwistMax.RadianToDegree();
+				}
+
+				joint = hingeJoint;
+			}
+			else if ( jointDesc.Type == PhysicsGroupDescription.JointType.Ball )
+			{
+				var ballJoint = body.AddComponent<BallJoint>();
+				ballJoint.Attachment = BallJoint.AttachmentMode.LocalFrames;
+				ballJoint.LocalFrame1 = jointDesc.Frame1;
+				ballJoint.LocalFrame2 = jointDesc.Frame2;
+
+				if ( jointDesc.EnableSwingLimit )
+				{
+					ballJoint.SwingLimitEnabled = true;
+					ballJoint.SwingLimit = new Vector2( jointDesc.SwingMin.RadianToDegree(), jointDesc.SwingMax.RadianToDegree() );
+				}
+
+				if ( jointDesc.EnableTwistLimit )
+				{
+					ballJoint.TwistLimitEnabled = true;
+					ballJoint.TwistLimit = new Vector2( jointDesc.TwistMin.RadianToDegree(), jointDesc.TwistMax.RadianToDegree() );
+				}
+
+				joint = ballJoint;
+			}
+			else if ( jointDesc.Type == PhysicsGroupDescription.JointType.Fixed )
+			{
+				var fixedJoint = body.AddComponent<FixedJoint>();
+
+				joint = fixedJoint;
+			}
+			else if ( jointDesc.Type == PhysicsGroupDescription.JointType.Slider )
+			{
+				var sliderJoint = body.AddComponent<SliderJoint>();
+
+				joint = sliderJoint;
+			}
+
+			if ( joint.IsValid() )
+			{
+				joint.Body = _bodies[jointDesc.Body2].Body;
+				joint.EnableCollision = jointDesc.EnableCollision;
+				joint.BreakForce = jointDesc.LinearStrength;
+				joint.BreakTorque = jointDesc.AngularStrength;
+			}
+		}
+
+		foreach ( var pair in _bodies )
+		{
+			pair.Body.Enabled = true;
+		}
+	}
+
+	private void DestroyPhysics()
+	{
+		foreach ( var pair in _bodies )
+		{
+			if ( !pair.Body.IsValid() )
+				continue;
+
+			pair.Body.Destroy();
+		}
+
+		_bodies.Clear();
+
+		if ( Renderer.IsValid() )
+		{
+			Renderer.ClearPhysicsBones();
+		}
+	}
+
+	private void PositionRendererBonesFromPhysics()
+	{
+		if ( !Renderer.IsValid() )
+			return;
+
+		var world = WorldTransform;
+
+		foreach ( var pair in _bodies )
+		{
+			if ( !pair.Body.IsValid() )
+				continue;
+
+			var local = world.ToLocal( pair.Body.WorldTransform );
+			Renderer.SetBoneTransform( pair.Bone, local );
+		}
+	}
+
+	protected override void OnUpdate()
+	{
+		if ( !Renderer.IsValid() )
+			return;
+
+		PositionRendererBonesFromPhysics();
+	}
+
+	protected override void OnEnabled()
+	{
+		base.OnEnabled();
+
+		CreatePhysics();
+	}
+
+	protected override void OnDisabled()
+	{
+		base.OnDisabled();
+
+		DestroyPhysics();
+	}
+}
