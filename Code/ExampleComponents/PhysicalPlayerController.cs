d35e138a968447b156f43977b6046f669a94ec14
Garry Newman <garrynewman@users.noreply.github.com>
2024-10-19 08:59:11 +0100

    Tweak pressure/gib calcs

--- Code/ExampleComponents/PhysicalPlayerController.cs
+++ Code/ExampleComponents/PhysicalPlayerController.cs
@@ -258,26 +258,52 @@ public class PhysicalPlayerController : Component, Component.ICollisionListener
 	}
 
 
+	float nominalPressure => 350;
 	float pressure;
+	int i = 0;
 
 	void ICollisionListener.OnCollisionStart( Collision collision )
 	{
-		pressure += collision.Contact.Impulse;
+		//if ( float.IsNaN( collision.Contact.Impulse ) ) return;
+
+		//var imp = collision.Contact.Impulse / collision.Self.Body.Mass;
+
+		//if ( imp < nominalPressure ) return;
+		//if ( collision.Contact.NormalSpeed < 0 ) return;
+
+		//DebugDrawSystem.Current.AddText( collision.Contact.Point + Vector3.Up * i * 16, $"[{imp:n0}] {collision.Contact.Normal} {collision.Contact.NormalSpeed} {collision.Other.Collider}" );
+		//pressure += imp;
+		//i++;
 	}
 	void ICollisionListener.OnCollisionUpdate( Collision collision )
 	{
-		pressure += collision.Contact.Impulse;
+		if ( float.IsNaN( collision.Contact.Impulse ) ) return;
+
+		var imp = collision.Contact.Impulse / collision.Self.Body.Mass;
+
+		if ( imp < nominalPressure ) return;
+
+		//DebugDrawSystem.Current.AddText( collision.Contact.Point + Vector3.Up * i * 16, $"[{imp:n0}] {collision.Other.Collider}" );
+		//DebugDrawSystem.Current.AddText( collision.Contact.Point, $"{collision.Contact.Impulse}" ).WithTime( 10.0f );
+
+		pressure += imp;
+		i++;
 	}
 
 	void UpdatePressure()
 	{
-		pressure -= 1000000;
-		if ( pressure < 0 ) pressure = 0;
+		i = 0;
 
 		//if ( pressure > 100000 )
-		//DebugDrawSystem.Current.AddText( WorldPosition + Vector3.Up * 80, $"pressure: {pressure}" );
+		//DebugDrawSystem.Current.AddText( WorldPosition + Vector3.Up * 80, $"pressure: {pressure}\nvelocity: {Controller.Body.Velocity}" );
 
-		if ( pressure > 500000 )
+		if ( pressure > 1000 )
+		{
+			Log.Info( $"Explode pressure: {pressure}" );
 			Explode();
+		}
+
+		pressure -= 1000;
+		if ( pressure < 0 ) pressure = 0;
 	}
 }
