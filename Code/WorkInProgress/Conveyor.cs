4238e65142e0c3b9761aea05ca0d7720420b12d3
Layla <fortblox@gmail.com>
2024-10-19 23:35:36 +0100

    Update Conveyor component to set collider surface velocity, this component doesn't really need to exist anymore though

--- Code/WorkInProgress/Conveyor.cs
+++ Code/WorkInProgress/Conveyor.cs
@@ -1,39 +1,21 @@
+namespace Sandbox;
+
 /// <summary>
-/// This is all wrong. At this point it might as well be a trigger and go through the physics objects.
+/// Sets velocity on a collider to simulate a conveyor belt
 /// </summary>
-public sealed class Conveyor : Component, IScenePhysicsEvents
+public sealed class Conveyor : Component
 {
 	[Property]
 	public Vector3 Velocity { get; set; }
 
-	Vector3 pos;
-	Rigidbody rBody;
-
-	protected override void OnStart()
+	protected override void OnFixedUpdate()
 	{
-		base.OnStart();
+		base.OnFixedUpdate();
 
-
-	}
-
-	void IScenePhysicsEvents.PrePhysicsStep()
-	{
-		pos = WorldPosition;
-
-		rBody = GetComponent<Rigidbody>();
-
-		if ( rBody.IsValid() )
+		var collider = GetComponent<Collider>();
+		if ( collider.IsValid() )
 		{
-			rBody.Velocity = WorldRotation * Velocity;
-			rBody.PhysicsBody.Sleeping = false;
-		}
-	}
-
-	void IScenePhysicsEvents.PostPhysicsStep()
-	{
-		if ( rBody.IsValid() )
-		{
-			rBody.WorldPosition = pos;
+			collider.SurfaceVelocity = Velocity;
 		}
 	}
 }
