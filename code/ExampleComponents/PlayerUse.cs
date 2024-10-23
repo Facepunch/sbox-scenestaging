9c0046488e05451ca5f72ee2d7da1c15e3069961
bakscratch <lt_8@hotmail.co.uk>
2024-10-23 19:05:14 +0100

    Hammer GameObject Scene
    (You'll need to compile)

---
+++ code/ExampleComponents/PlayerUse.cs
@@ -0,0 +1,46 @@
+using Sandbox;
+
+public sealed class PlayerUse : Component
+{
+	CameraComponent camera { get; set; }
+
+	[Property] SoundEvent useSound { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		if ( IsProxy ) return;
+
+		camera = Scene.Camera;
+	}
+
+	protected override void OnUpdate()
+	{
+		if ( IsProxy ) return;
+
+		if ( !camera.IsValid() ) return;
+
+		if ( Input.Pressed( "use" ) )
+		{
+			Use();
+		}
+	}
+
+	void Use()
+	{
+		var tr = Scene.Trace.Ray( camera.WorldPosition, camera.WorldPosition + camera.WorldRotation.Forward * 100 )
+	.IgnoreGameObject( GameObject )
+	.Run();
+
+		if ( tr.Hit )
+		{
+			Sound.Play( useSound );
+
+			if(tr.GameObject.Components.Get<BaseInteractor>() is BaseInteractor interactable )
+			{
+				interactable.OnUsed();
+			}
+		}
+	}
+}
