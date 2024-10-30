e4c35e3e151a1e4f61586a5425a5a083b5a7d601
Garry Newman <garrynewman@users.noreply.github.com>
2024-10-30 15:31:03 +0000

    Dresser scene + component

--- code/ExampleComponents/ApplyLocalClothing.cs
+++ code/ExampleComponents/ApplyLocalClothing.cs
@@ -1,13 +1,64 @@
 namespace Sandbox;
 
-public sealed class ApplyLocalClothing : Component
+[Alias( "Sandbox.ApplyLocalClothing" )]
+public sealed class Dresser : Component, Component.ExecuteInEditor
 {
 	[Property]
 	public SkinnedModelRenderer BodyTarget { get; set; }
 
+	[Property]
+	public bool ApplyLocalUserClothes { get; set; } = true;
+
+	[Property]
+	public bool ApplyHeightScale { get; set; } = true;
+
+	[Property]
+	public List<ClothingContainer.ClothingEntry> Clothing { get; set; }
+
 	protected override void OnAwake()
 	{
-		var clothing = ClothingContainer.CreateFromLocalUser();
+		if ( IsProxy )
+			return;
+
+		Apply();
+	}
+
+	void Apply()
+	{
+		if ( !BodyTarget.IsValid() )
+			return;
+
+		var clothing = ApplyLocalUserClothes ? ClothingContainer.CreateFromLocalUser() : new ClothingContainer();
+
+		if ( !ApplyHeightScale )
+			clothing.Height = 1;
+
+		if ( Clothing is not null )
+		{
+			clothing.AddRange( Clothing );
+		}
+
+		clothing.Normalize();
+
 		clothing.Apply( BodyTarget );
+
+		BodyTarget.PostAnimationUpdate();
+	}
+
+	protected override void OnValidate()
+	{
+		if ( IsProxy )
+			return;
+
+		base.OnValidate();
+
+		using var p = Scene.Push();
+
+		if ( !BodyTarget.IsValid() )
+		{
+			BodyTarget = GetComponentInChildren<SkinnedModelRenderer>();
+		}
+
+		Apply();
 	}
 }
