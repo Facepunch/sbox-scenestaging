85ea382213b00f28e25cfb493e48f289215ad8c3
Garry Newman <garrynewman@users.noreply.github.com>
2024-12-03 09:43:04 +0000

    Fix up GroundTruthAmbientOcclusion

--- Code/WorkInProgress/AmbientOcclusionGTAO.cs
+++ Code/WorkInProgress/AmbientOcclusionGTAO.cs
@@ -2,17 +2,21 @@ using Sandbox.Rendering;
 
 namespace Sandbox;
 
-[Title( "Ground Truth Ambient Occlusion (Staging)" )]
+/// <summary>
+/// Screen space ambient occlusion (SSAO)
+/// </summary>
+[Title( "SSAO" )]
 [Category( "Post Processing" )]
 [Icon( "contrast" )]
-public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.ExecuteInEditor
+[Alias( "GroundTruthAmbientOcclusion" )]
+public sealed class SSAO : PostProcess, Component.ExecuteInEditor
 {
 	public enum SampleQuality
 	{
 		/// <summary>
 		/// 9 samples
 		/// </summary>
-		[Icon("workspaces")]
+		[Icon( "workspaces" )]
 		Low,
 		/// <summary>
 		/// 16 samples
@@ -26,27 +30,37 @@ public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.Execute
 		High
 	}
 
-	//-------------------------------------------------------------------------
+	/// <summary>
+	/// The intensity of the darkening effect. Has no impact on performance.
+	/// </summary>
+	[Property, Range( 0, 1 ), Category( "Properties" )] public float Intensity { get; set; } = 0.15f;
+
+	/// <summary>
+	/// Maximum distance of samples from pixel when determining its occlusion, in world units.
+	/// </summary>
+	[Property, Range( 1, 512 ), Category( "Properties" )] public int Radius { get; set; } = 128;
+
+	/// <summary>
+	/// Gently reduce sample impact as it gets out of the effect's radius bounds
+	/// </summary>
+	[Property, Range( 0.01f, 1.0f ), Category( "Properties" )] public float FalloffRange { get; set; } = 0.75f;
 
 	/// <summary>
 	/// Number of pixel samples taken to determine occlusion
 	/// </summary>
-	[Property, Category("Quality")] public SampleQuality Quality { get; set; } = SampleQuality.High;
-	/// <summary>
-	/// The intensity of the darkening effect. Has no impact on performance.
-	/// </summary>
-	[Property, Range( 0, 16, 0.1f ), Category("Properties")] public float Intensity { get; set; } = 8.00f;
-	/// <summary>
-	/// Maximum distance of samples from pixel when determining its occlusion
-	/// </summary>
-	[Property, Range( 1, 512 ), Category("Properties")] public int Radius { get; set; } = 512;
-	/// <summary>
-	/// The distance at which the effect starts to fade out
-	/// </summary>
-	[Property, Range( 0.01f, 1.0f ), Category("Properties")] public float FalloffDistance { get; set; } = 1.0f;
+	[Property, Category( "Quality" )] public SampleQuality Quality { get; set; } = SampleQuality.High;
 
-	//-------------------------------------------------------------------------
-	
+	/// <summary>
+	/// How we should denoise the effect
+	/// </summary>
+	[Property, Category( "Quality" )] public DenoiseModes DenoiseMode { get; set; } = DenoiseModes.Temporal;
+
+	/// <summary>
+	/// Slightly reduce impact of samples further back to counter the bias from depth-based (incomplete) input scene geometry data
+	/// </summary>
+	[Property, Category( "Quality" ), Range( 0.0f, 0.5f )] public float ThinCompensation { get; set; } = 0.0f;
+
+
 	Rendering.CommandList commands;
 
 	protected override void OnEnabled()
@@ -62,7 +76,7 @@ public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.Execute
 		Camera.RemoveCommandList( commands );
 		commands = null;
 	}
-    int Frame = 0;
+	int Frame = 0;
 
 	private struct GTAOConstants
 	{
@@ -87,48 +101,26 @@ public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.Execute
 		public float ThinOccluderCompensation = 0.0f;    // the new 'thickness heuristic' approach
 		public float DepthMIPSamplingOffset = 3.30f;     // main trade-off between performance (memory bandwidth) and quality (temporal stability is the first affected, thin objects next)
 		public int NoiseIndex = 0;            // frameIndex % 64 if using TAA or 0 otherwise
-		public GTAOConstants() {}
+		public GTAOConstants() { }
 	};
 
 	enum GTAOPasses
 	{
 		ViewDepthChain,    // XeGTAO depth filter does average depth, a bit similar to our depth chain
-        MainPass,
-        DenoiseSpatial,
-        DenoiseTemporal
+		MainPass,
+		DenoiseSpatial,
+		DenoiseTemporal
 	}
-	
+
 	GTAOConstants GetGTAOConstants( /*SceneCamera camera*/ )
 	{
 		var consts = new GTAOConstants();
-		/*consts.ViewportSize = viewportSize;
-		consts.ViewportPixelSize = new Vector2( 1.0f / (float)viewportSize.x, 1.0f / (float)viewportSize.y );
-
-		float depthLinearizeMul = ( camera.ZFar * camera.ZNear ) / ( camera.ZFar - camera.ZNear );
-		float depthLinearizeAdd = camera.ZFar / ( camera.ZFar - camera.ZNear );
-
-		if ( depthLinearizeMul * depthLinearizeAdd < 0 )
-			depthLinearizeAdd = -depthLinearizeAdd;
-		consts.DepthUnpackConsts = new( depthLinearizeMul, depthLinearizeAdd );
-
-		float aspectRatio = (float)viewportSize.x / (float)viewportSize.y;
-		float tanHalfFOVY = MathF.Tan(0.5f * camera.FieldOfView * (MathF.PI / 180.0f));
-		float tanHalfFOVX = tanHalfFOVY * aspectRatio;
-		consts.CameraTanHalfFOV = new Vector2(tanHalfFOVX, tanHalfFOVY);
-
-		consts.NDCToViewMul = new( consts.CameraTanHalfFOV.x * 2.0f, consts.CameraTanHalfFOV.y * -2.0f );
-		consts.NDCToViewAdd = new( consts.CameraTanHalfFOV.x * -1.0f, consts.CameraTanHalfFOV.y * 1.0f );
-
-		consts.NDCToViewMul_x_PixelSize = new( consts.NDCToViewMul.x * consts.ViewportPixelSize.x, consts.NDCToViewMul.y * consts.ViewportPixelSize.y );*/
 
 		// The above is calculated on shader now
 		consts.ViewportSize = Vector2Int.Zero;
 		consts.ViewportPixelSize = Vector2.Zero;
-
 		consts.DepthUnpackConsts = Vector2.Zero;
-
 		consts.CameraTanHalfFOV = Vector2.Zero;
-
 		consts.NDCToViewMul = Vector2.Zero;
 		consts.NDCToViewAdd = Vector2.Zero;
 
@@ -137,14 +129,14 @@ public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.Execute
 		//-------------------------------------------------------------------------
 		consts.EffectRadius = Radius;
 
-		consts.EffectFalloffRange = FalloffDistance;
+		consts.EffectFalloffRange = FalloffRange;
 		consts.DenoiseBlurBeta = 1.2f; // Used only on Spatial denoising
-		
-		consts.NoiseIndex = DenoiseMode == DenoiseModes.Temporal ? Frame % 64 : 0;
-		consts.ThinOccluderCompensation = 1.0f / Thickness;
-		consts.FinalValuePower = Intensity * 0.1375f; // Normalizing to the 16.0f from previous implementation
 
-		switch (Quality)
+		consts.NoiseIndex = DenoiseMode == DenoiseModes.Temporal ? Frame % 64 : 0;
+		consts.ThinOccluderCompensation = ThinCompensation;
+		consts.FinalValuePower = Intensity * 5.0f;
+
+		switch ( Quality )
 		{
 			case SampleQuality.Low:
 				consts.TAABlendAmount = 0.95f;
@@ -167,41 +159,37 @@ public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.Execute
 		/// Applies spatial denoising to reduce noise by averaging pixel values within a local neighborhood.
 		/// This method smooths out noise by considering the spatial relationship between pixels in a single frame.
 		/// </summary>
-		[Icon("filter_center_focus")]
+		[Icon( "filter_center_focus" )]
 		Spatial,
 
 		/// <summary>
 		/// Applies temporal denoising to reduce noise by averaging pixel values over multiple frames.
 		/// This method leverages the temporal coherence of consecutive frames to achieve a noise-free result.
 		/// </summary>
-		[Icon("auto_awesome_motion")]
+		[Icon( "auto_awesome_motion" )]
 		Temporal
 	}
 
 	protected override void OnUpdate()
 	{
-        Rebuild();
+		Rebuild();
 	}
-	/// <summary>
-	/// How we should denoise the effect
-	/// </summary>
-	[Property, Category("Quality") ] public DenoiseModes DenoiseMode { get; set; } = DenoiseModes.Spatial;
-	[Property, Category("Quality"), Range( 0.0f, 1.0f ) ] public float Thickness { get; set; } = 0.2f;
+
 	void Rebuild()
 	{
 		if ( commands is null )
 			return;
 
 		commands.Reset();
-		
-		RenderTargetHandle ViewDepthChainTexture 	= commands.GetRenderTarget( "ViewDepthChainTexture", ImageFormat.R32F, numMips: 5 );
-		RenderTargetHandle WorkingEdgesTexture 		= commands.GetRenderTarget( "WorkingEdgesTexture", ImageFormat.R16F );
-		RenderTargetHandle WorkingAOTexture 		= commands.GetRenderTarget( "WorkingAOTexture", ImageFormat.A8 );
-		RenderTargetHandle AOTexture0 				= commands.GetRenderTarget( "AOTexture0", ImageFormat.A8 );
-		RenderTargetHandle AOTexture1 				= commands.GetRenderTarget( "AOTexture1", ImageFormat.A8 );
+
+		RenderTargetHandle ViewDepthChainTexture = commands.GetRenderTarget( "ViewDepthChainTexture", ImageFormat.R32F, numMips: 5 );
+		RenderTargetHandle WorkingEdgesTexture = commands.GetRenderTarget( "WorkingEdgesTexture", ImageFormat.R16F );
+		RenderTargetHandle WorkingAOTexture = commands.GetRenderTarget( "WorkingAOTexture", ImageFormat.A8 );
+		RenderTargetHandle AOTexture0 = commands.GetRenderTarget( "AOTexture0", ImageFormat.A8 );
+		RenderTargetHandle AOTexture1 = commands.GetRenderTarget( "AOTexture1", ImageFormat.A8 );
 
 		bool pingPong = (Frame++ % 2) == 0;
-		
+
 		var AOTextureCurrent = pingPong ? AOTexture0 : AOTexture1;
 		var AOTexturePrev = pingPong ? AOTexture1 : AOTexture0;
 
@@ -211,28 +199,28 @@ public sealed class GroundTruthAmbientOcclusion : PostProcess, Component.Execute
 
 		// 
 		// Bind textures to the compute shader
-		commands.Set("WorkingDepthMIP0", 	ViewDepthChainTexture.ColorTexture, 0);
-		commands.Set("WorkingDepthMIP1", 	ViewDepthChainTexture.ColorTexture, 1);
-		commands.Set("WorkingDepthMIP2", 	ViewDepthChainTexture.ColorTexture, 2);
-		commands.Set("WorkingDepthMIP3", 	ViewDepthChainTexture.ColorTexture, 3);
-		commands.Set("WorkingDepthMIP4", 	ViewDepthChainTexture.ColorTexture, 4);
-		commands.Set("WorkingDepth", 		ViewDepthChainTexture.ColorTexture);
-		commands.Set("WorkingAOTerm", 		WorkingAOTexture.ColorTexture);
-		commands.Set("WorkingEdges", 		WorkingEdgesTexture.ColorTexture);
-		commands.Set("FinalAOTerm", 		AOTextureCurrent.ColorTexture);
-		commands.Set("FinalAOTermPrev", 	AOTexturePrev.ColorTexture);
+		commands.Set( "WorkingDepthMIP0", ViewDepthChainTexture.ColorTexture, 0 );
+		commands.Set( "WorkingDepthMIP1", ViewDepthChainTexture.ColorTexture, 1 );
+		commands.Set( "WorkingDepthMIP2", ViewDepthChainTexture.ColorTexture, 2 );
+		commands.Set( "WorkingDepthMIP3", ViewDepthChainTexture.ColorTexture, 3 );
+		commands.Set( "WorkingDepthMIP4", ViewDepthChainTexture.ColorTexture, 4 );
+		commands.Set( "WorkingDepth", ViewDepthChainTexture.ColorTexture );
+		commands.Set( "WorkingAOTerm", WorkingAOTexture.ColorTexture );
+		commands.Set( "WorkingEdges", WorkingEdgesTexture.ColorTexture );
+		commands.Set( "FinalAOTerm", AOTextureCurrent.ColorTexture );
+		commands.Set( "FinalAOTermPrev", AOTexturePrev.ColorTexture );
 
 		commands.SetCombo( "D_QUALITY", Quality );
-		
+
 		// View depth chain
 		{
-			commands.SetCombo( "D_PASS",  GTAOPasses.ViewDepthChain );
+			commands.SetCombo( "D_PASS", GTAOPasses.ViewDepthChain );
 			commands.DispatchCompute( csAO, AOTextureCurrent.Size );
 		}
 
 		// Main pass
 		{
-			commands.SetCombo( "D_PASS",  GTAOPasses.MainPass );
+			commands.SetCombo( "D_PASS", GTAOPasses.MainPass );
 			commands.DispatchCompute( csAO, AOTextureCurrent.Size );
 		}
 
