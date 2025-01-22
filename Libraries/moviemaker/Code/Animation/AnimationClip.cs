ed8b352e8546d6d9c30f0f088a3a3a026a9108a9
James King <james.king@facepunchstudios.com>
2025-01-22 16:14:52 +0000

    AnimationClip / AnimationTrack

---
+++ Libraries/moviemaker/Code/Animation/AnimationClip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sandbox.Animation;
+
+#nullable enable
+
+/// <summary>
+/// Contains a list of tracks that describe changes over time.
+/// </summary>
+public sealed class AnimationClip
+{
+	private readonly List<AnimationTrack> _tracks = new();
+
+	/// <summary>
+	/// List of animation tracks in this clip.
+	/// </summary>
+	public IReadOnlyList<AnimationTrack> Tracks => _tracks;
+
+	/// <summary>
+	/// Adds a new animation track to this clip, with the given type.
+	/// </summary>
+	/// <param name="type">Property type for this track.</param>
+	public AnimationTrack AddTrack( Type type )
+	{
+		var nextId = _tracks.Count == 0 ? 1 : _tracks[^1].Id + 1;
+		var track = new AnimationTrack( this, nextId, type );
+
+		_tracks.Add( track );
+
+		return track;
+	}
+
+	/// <summary>
+	/// Should only be called from <see cref="AnimationTrack.Remove"/>
+	/// </summary>
+	internal void RemoveTrackInternal( AnimationTrack animationTrack )
+	{
+		_tracks.Remove( animationTrack );
+	}
+}
---
+++ Libraries/moviemaker/Code/Animation/AnimationTrack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Sandbox.Animation;
+
+#nullable enable
+
+/// <summary>
+/// A track from an <see cref="AnimationClip"/> that describes a value that can change over time.
+/// </summary>
+public class AnimationTrack : IValid
+{
+	private AnimationClip? _clip;
+
+	/// <summary>
+	/// Which clip contains this track.
+	/// </summary>
+	public AnimationClip Clip => _clip ?? throw new Exception( $"{nameof(AnimationTrack)} '{DisplayName}' has been removed." );
+
+	/// <summary>
+	/// ID number for referencing this track, unique in the containing clip.
+	/// </summary>
+	public int Id { get; }
+
+	/// <summary>
+	/// User-facing name of this track.
+	/// </summary>
+	public string DisplayName { get; set; }
+
+	/// <summary>
+	/// What type of property is this track.
+	/// </summary>
+	public Type Type { get; }
+
+	/// <summary>
+	/// False if this track has been removed.
+	/// </summary>
+	public bool IsValid => _clip is not null;
+
+	internal AnimationTrack( AnimationClip clip, int id, Type type )
+	{
+		_clip = clip;
+
+		Id = id;
+		Type = type;
+
+		// Placeholder name
+
+		DisplayName = $"Track {id}";
+	}
+
+	/// <summary>
+	/// Remove this track from the clip.
+	/// </summary>
+	public void Remove()
+	{
+		_clip?.RemoveTrackInternal( this );
+		_clip = null;
+	}
+}
