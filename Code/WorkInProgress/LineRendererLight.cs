3cef85735b93ba9b5395df884dc0ef0df0a66aee
Sam Pavlovic <sam@sampavlovic.com>
2025-03-26 23:05:57 -0300

    Line Renderer Light subcomponent https://files.facepunch.com/sampavlovic/1b2611b1/b20gTMwYSi.png

---
+++ Code/WorkInProgress/LineRendererLight.cs
@@ -0,0 +1,71 @@
+namespace Sandbox;
+
+[Title("Line Renderer Light (Staging)")]
+public class LineRendererLight : Component, Component.ExecuteInEditor
+{
+    LineRenderer Renderer;
+    SceneLight so;
+
+    [Property] public float Brightness { get; set; } = 1.0f;
+    
+    public LineRendererLight()
+    {
+    }
+
+    protected override void OnEnabled()
+    {
+        Renderer = Components.GetInAncestorsOrSelf<LineRenderer>();
+
+        if (Renderer is null)
+        {
+            Log.Warning($"No line renderer component found for {this}");
+            return;
+        }
+
+        // Clean up any existing light
+        if (so.IsValid())
+            so.Delete();
+            
+        so = new SceneLight(Renderer.Scene.SceneWorld, Vector3.Zero, 100, Color.Red);
+    }
+
+	protected override void OnDisabled()
+	{
+		if ( !so.IsValid() ) return;
+		so.Delete();
+	}
+
+    protected override void OnUpdate()
+    {
+        if (!so.IsValid() || Renderer == null) return;
+        
+        // Skip update if renderer has no points
+        bool useVectorPoints = Renderer.UseVectorPoints;
+        if ((useVectorPoints && (Renderer.VectorPoints == null || Renderer.VectorPoints.Count == 0)) ||
+            (!useVectorPoints && (Renderer.Points == null || Renderer.Points.Count == 0)))
+            return;
+        
+        Vector3 p1, p2;
+
+        if (useVectorPoints)
+        {
+            p1 = Renderer.VectorPoints.First();
+            p2 = Renderer.VectorPoints.Last();
+        }
+        else
+        {
+            p1 = Renderer.Points.First().WorldPosition;
+            p2 = Renderer.Points.Last().WorldPosition;
+        }
+
+        float distance = p1.Distance(p2);
+        
+        
+        so.Position = ( p1 + p2 ) / 2.0f;
+        so.Radius = 256 + p1.Distance( p2 );
+		so.LightColor = Renderer.Color.Evaluate( 0.5f ) * Brightness;
+		so.ShapeSize = new Vector2( p1.Distance( p2 ), Renderer.Width.Evaluate( 0.5f ) * 0.5f );
+		so.Rotation = Rotation.LookAt( p2 - p1 );
+		so.Shape = SceneLight.LightShape.Capsule;
+    }
+}
