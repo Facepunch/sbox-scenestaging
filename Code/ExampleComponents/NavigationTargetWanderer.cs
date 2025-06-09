ae94da6698e68441b372a16e40db2bb044b0fb22
Lorenz Junglas <4759511+lolleko@users.noreply.github.com>
2025-06-09 18:00:45 +0200

    Navlink scene (#142)
    * WIP scene that tests navlinks
    
    * Add some really simple jumping animations to nav test scene
    
    https://files.facepunch.com/lolleko/2025/January/06_17-14-NotableChanticleer.mp4
    
    * Update test wanderer navlink callbacks
    
    * Remove unused NavLinkUpdate callback
    
    * Test physics based nav agent jumps
    
    https://files.facepunch.com/lolleko/2025/March/03_12-27-RecklessTattler.mp4
    
    * Test Ladders
    
    https://files.facepunch.com/lolleko/2025/March/03_17-53-AncientWalrus.mp4
    
    * NavLink API Cleanup
    
    * Cleanup nav link sample
    
    * CompleteLink -> CompleteLinkTraversal
    
    * Add missing end velocity to ladder sample
    
    * Remove velocity calculation for link samples
    
    * Remove redundant SetAgentPosition
    
    * Update nav link prefab and scene description
    
    * Rename scene title

--- Code/ExampleComponents/NavigationTargetWanderer.cs
+++ Code/ExampleComponents/NavigationTargetWanderer.cs
@@ -3,47 +3,253 @@ using System;
 
 public sealed class NavigationTargetWanderer : Component
 {
-    [Property]
-    public List<GameObject> PotentialTargets { get; set; }
+	[Property]
+	public List<GameObject> PotentialTargets { get; set; }
 
-    [RequireComponent]
-    NavMeshAgent Agent { get; set; }
+	[RequireComponent]
+	NavMeshAgent Agent { get; set; }
 
-    [Property]
-    public SkinnedModelRenderer Body { get; set; }
+	[RequireComponent]
+	Rigidbody Body { get; set; }
 
-    private Vector3 _currentTarget = Vector3.Zero;
+	[RequireComponent]
+	public SkinnedModelRenderer Model { get; set; }
 
-    protected override void OnEnabled()
-    {
-        _currentTarget = PotentialTargets[Random.Shared.Next(0, PotentialTargets.Count)].WorldPosition;
-    }
+	private Vector3 _currentTarget = Vector3.Zero;
 
-    protected override void OnFixedUpdate()
-    {
-        var dir = Agent.Velocity;
-        var forward = WorldRotation.Forward.Dot(dir);
-        var sideward = WorldRotation.Right.Dot(dir);
+	private TimeSince _timeSinceLastTargetChange = 0;
 
-        var angle = MathF.Atan2(sideward, forward).RadianToDegree().NormalizeDegrees();
+	protected override void OnEnabled()
+	{
+		_currentTarget = PotentialTargets[Random.Shared.Next( 0, PotentialTargets.Count )].WorldPosition;
+		Agent.MoveTo( _currentTarget );
 
-        Body.Set("move_direction", angle);
-        Body.Set("move_speed", Agent.Velocity.Length);
-        Body.Set("move_groundspeed", Agent.Velocity.WithZ(0).Length);
-        Body.Set("move_y", sideward);
-        Body.Set("move_x", forward);
-        Body.Set("move_z", Agent.Velocity.z);
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
 
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		if ( agent == null )
+			return;
 
-        if (agent == null)
-            return;
+		agent.MoveTo( _currentTarget );
+	}
 
-        agent.MoveTo(_currentTarget);
 
-        if (WorldPosition.WithZ(0).Distance(_currentTarget.WithZ(0)) < 32f)
-        {
-            _currentTarget = PotentialTargets[Random.Shared.Next(0, PotentialTargets.Count)].WorldPosition;
-        }
-    }
+	protected override void OnUpdate()
+	{
+		var dir = Agent.Velocity;
+		var forward = WorldRotation.Forward.Dot( dir );
+		var sideward = WorldRotation.Right.Dot( dir );
+
+		var angle = MathF.Atan2( sideward, forward ).RadianToDegree().NormalizeDegrees();
+
+		Model.Set( "move_direction", angle );
+		Model.Set( "move_speed", Agent.Velocity.Length );
+		Model.Set( "move_groundspeed", Agent.Velocity.WithZ( 0 ).Length );
+		Model.Set( "move_y", sideward );
+		Model.Set( "move_x", forward );
+		Model.Set( "move_z", Agent.Velocity.z );
+
+		Model.Set( "wish_x", Agent.WishVelocity.x );
+		Model.Set( "wish_y", Agent.WishVelocity.y );
+		Model.Set( "wish_z", Agent.WishVelocity.z );
+
+		if ( _timeSinceLastTargetChange > 20f || WorldPosition.WithZ( 0 ).Distance( _currentTarget.WithZ( 0 ) ) < 16f )
+		{
+			_currentTarget = PotentialTargets[Random.Shared.Next( 0, PotentialTargets.Count )].WorldPosition;
+			Agent.MoveTo( _currentTarget );
+			_timeSinceLastTargetChange = 0;
+		}
+	}
+}
+
+public sealed class NavigationLinkTraversal : Component
+{
+	[RequireComponent]
+	NavMeshAgent Agent { get; set; }
+
+	[RequireComponent]
+	Rigidbody Body { get; set; }
+
+	[RequireComponent]
+	public SkinnedModelRenderer Model { get; set; }
+
+	protected override void OnEnabled()
+	{
+		Agent.AutoTraverseLinks = false;
+
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+		if ( agent == null )
+			return;
+
+		agent.LinkEnter += OnNavLinkEnter;
+	}
+
+	protected override void OnDisabled()
+	{
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		if ( agent == null )
+			return;
+
+		agent.LinkEnter -= OnNavLinkEnter;
+	}
+
+	private void OnNavLinkEnter()
+	{
+		// If link is a ladder and we are going up, climb it
+		if ( Agent.CurrentLinkTraversal.Value.LinkComponent.Tags.Has( "ladder" ) &&
+			Agent.CurrentLinkTraversal.Value.LinkExitPosition.z > WorldPosition.z )
+		{
+			ClimbLadder();
+		}
+		else
+		{
+			// 50/50 chance of physics jump or paraobolic jump
+			if ( Random.Shared.Next( 0, 2 ) == 0 )
+				PhysicsJump();
+			else
+				ParabolicJump();
+		}
+	}
+
+	private async void ClimbLadder()
+	{
+		var initialPos = Agent.CurrentLinkTraversal.Value.AgentInitialPosition;
+
+		var start = Agent.CurrentLinkTraversal.Value.LinkEnterPosition;
+		var endVertical = start.WithZ( Agent.CurrentLinkTraversal.Value.LinkExitPosition.z );
+		var end = Agent.CurrentLinkTraversal.Value.LinkExitPosition;
+
+		var climbSpeed = 100f;
+
+		var startDuration = (start - initialPos).Length / climbSpeed;
+		var climbDuration = (endVertical - start).Length / climbSpeed;
+		var endDuration = (end - endVertical).Length / climbSpeed;
+
+		var totalLadderTime = startDuration + climbDuration + endDuration;
+
+		TimeSince timeSinceStart = 0;
+
+		while ( timeSinceStart < totalLadderTime )
+		{
+			Vector3 newPosition = start;
+
+			// 1. Make sure we are positioned at the link start
+			if ( timeSinceStart < startDuration )
+			{
+				newPosition = Vector3.Lerp( initialPos, start, timeSinceStart / startDuration );
+			}
+			// 2. Vertical Movement
+			else if ( timeSinceStart < startDuration + climbDuration )
+			{
+				newPosition = Vector3.Lerp( start, endVertical, (timeSinceStart - startDuration) / climbDuration );
+			}
+			// 3. Move off ladder to link end position
+			else
+			{
+				newPosition = Vector3.Lerp( endVertical, end, (timeSinceStart - startDuration - climbDuration) / endDuration );
+			}
+
+			Agent.SetAgentPosition( newPosition );
+
+			await Task.Frame();
+		}
+
+		Agent.SetAgentPosition( end );
+
+		Agent.CompleteLinkTraversal();
+	}
+
+	private async void ParabolicJump()
+	{
+		Model.Set( "b_grounded", false );
+		Model.Set( "b_jump", true );
+
+		var start = Agent.CurrentLinkTraversal.Value.AgentInitialPosition;
+		var end = Agent.CurrentLinkTraversal.Value.LinkExitPosition;
+
+		// Calculate peak height for the parabolic arc
+		var heightDifference = end.z - start.z;
+		var peakHeight = MathF.Abs( heightDifference ) + 25f;
+
+		var mid = (start + end) / 2f;
+
+		// Estimate prabolic duraion size using a triangle /\ between start, mid, end 
+		var startToMid = mid.WithZ( peakHeight ) - start;
+		var midToEnd = end - mid.WithZ( peakHeight );
+		var duration = ( startToMid + midToEnd ).Length / Agent.MaxSpeed;
+		duration = MathF.Max( 0.75f, duration ); // Ensure minimum duration
+
+		TimeSince timeSinceStart = 0;
+
+		while ( timeSinceStart < duration )
+		{
+			var t = timeSinceStart / duration;
+
+			// Linearly interpolate XY positions
+			var newPosition = Vector3.Lerp( start, end, t );
+
+			// Apply parabolic curve to Z position using a quadratic function
+			var yOffset = 4f * peakHeight * t * (1f - t);
+			newPosition.z = MathX.Lerp( start.z, end.z, t ) + yOffset;
+
+			Agent.SetAgentPosition( newPosition );
+
+			await Task.Frame();
+		}
+
+		Agent.SetAgentPosition( end );
+
+		Model.Set( "b_grounded", true );
+		Model.Set( "b_jump", false );
+
+		Agent.CompleteLinkTraversal();
+	}
+
+
+	private async void PhysicsJump()
+	{
+		Model.Set( "b_grounded", false );
+		Model.Set( "b_jump", true );
+
+		// Physiscs will drive our jump so disable game object position sync
+		Agent.UpdatePosition = false;
+
+		var start = Agent.CurrentLinkTraversal.Value.AgentInitialPosition;
+		var end = Agent.CurrentLinkTraversal.Value.LinkExitPosition;
+
+		var xyVelocity = Agent.MaxSpeed * (end.WithZ( 0 ) - start.WithZ( 0 )).Normal * 1.25f; 
+
+		var velocity = xyVelocity + Vector3.Up * Math.Max( 500f, (end.z - start.z) * 8f );
+		// Launch the agent into the air
+		Body.Velocity = velocity;
+
+		TimeSince timeSinceStart = 0;
+
+		while ( true )
+		{
+			Agent.SetAgentPosition( WorldPosition );
+
+			// Try to find ground
+			var tr = Scene.Trace.Ray( WorldPosition + Vector3.Up * 0.1f, WorldPosition + Vector3.Down * 1000 )
+				.IgnoreGameObjectHierarchy( GameObject )
+				.Run();
+
+			if ( tr.Hit && timeSinceStart > 0.5f && tr.Distance < 4f )
+			{
+				break;
+			}
+
+			await Task.Frame();
+		}
+
+		Agent.SetAgentPosition( WorldPosition );
+
+		// Hand back position control to the agent
+		Agent.UpdatePosition = true;
+
+		Model.Set( "b_grounded", true );
+		Model.Set( "b_jump", false );
+
+		Agent.CompleteLinkTraversal();
+	}
 }
