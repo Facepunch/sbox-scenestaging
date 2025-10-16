337d0b5d4cda5beca87b82bc1cecd28d72c5b6f1
a.pilote <antoinepilote1@hotmail.com>
2025-10-16 17:50:26 -0400

    Removed serialization stuff from t he component

--- Code/Components/ClutterComponent.cs
+++ Code/Components/ClutterComponent.cs
@@ -9,40 +9,7 @@ public sealed class ClutterComponent : Component, Component.ExecuteInEditor
 {
 	[Property] public List<ClutterLayer> Layers { get; set; } = [];
 
-	[Property, Hide]
-	public string SerializedData { get; set; }
-
 	public override string ToString() => GameObject.Name;
-
-	protected override void OnEnabled() => DeserializeData();
-	protected override void OnDestroy() => SerializeData();
-
-	/// <summary>
-	/// Serializes the current state to the SerializedData property
-	/// </summary>
-	public void SerializeData()
-	{
-		var json = ClutterSerializer.Serialize( this );
-		SerializedData = json.ToJsonString();
-	}
-
-	/// <summary>
-	/// Deserializes the clutter data
-	/// </summary>
-	private void DeserializeData()
-	{
-		try
-		{
-			if ( JsonNode.Parse( SerializedData ) is JsonObject json )
-			{
-				ClutterSerializer.Deserialize( this, json );
-			}
-		}
-		catch ( Exception ex )
-		{
-			Log.Warning( $"Failed to deserialize ClutterComponent: {ex.Message}" );
-		}
-	}
 
 	/// <summary>
 	/// Destroys a clutter instance if it's a valid prefab GameObject
