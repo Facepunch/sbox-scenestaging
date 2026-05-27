eb6e271251ebce26cd1436ca17215cefd7fa382b
Layla <fortblox@gmail.com>
2026-05-27 11:37:12 +0100

    2d example for callbacks on collision

--- Code/ExampleComponents/BrickBall.cs
+++ Code/ExampleComponents/BrickBall.cs
@@ -9,40 +9,45 @@ public sealed class BrickBall : Component, Component.ICollisionListener
 	[Range( 0, 100 )]
 	[Property] public float Speed { get; set; } = 100.0f;
 
+	/// <summary>
+	/// Normal of the plane the ball is locked to. Default is forward (XY plane).
+	/// </summary>
+	[Property] public Vector3 PlaneNormal { get; set; } = Vector3.Forward;
+
+	Vector3 Flatten( Vector3 v ) => v - Vector3.Dot( v, PlaneNormal ) * PlaneNormal;
+
 	protected override void DrawGizmos()
 	{
 		Gizmo.Draw.Line( 0, Direction.Normal * 100.0f );
 	}
 
-	float StartX;
+	float StartDepth;
 
 	protected override void OnStart()
 	{
 		base.OnStart();
 
-		StartX = WorldPosition.x;
+		StartDepth = Vector3.Dot( WorldPosition, PlaneNormal );
 
 		var rigidBody = Components.Get<Rigidbody>();
-		rigidBody.Velocity = (Direction.Normal * Speed).WithX( 0 );
+		rigidBody.Velocity = Flatten( Direction.Normal * Speed );
 	}
 
 	protected override void OnFixedUpdate()
 	{
-
-
 		LocalRotation = Rotation.Identity;
-		WorldPosition = WorldPosition.WithX( StartX );
+		WorldPosition = Flatten( WorldPosition ) + PlaneNormal * StartDepth;
 	}
 
 	public void OnCollisionStart( Collision o )
 	{
-		Direction = Vector3.Reflect( Direction, o.Contact.Normal ).Normal.WithX( 0 );
+		Direction = Flatten( Vector3.Reflect( Direction.Normal, o.Contact.Normal ) ).Normal;
 		o.Self.Body.Velocity = Direction * Speed;
 		o.Self.Body.AngularVelocity = 0;
 
 		if ( CollisionIndicator.IsValid() )
 		{
-			CollisionIndicator.Clone( o.Contact.Point);
+			CollisionIndicator.Clone( o.Contact.Point );
 		}
 	}
 
@@ -54,13 +59,13 @@ public sealed class BrickBall : Component, Component.ICollisionListener
 
 	public void OnCollisionUpdate( Collision o )
 	{
-		Direction = Vector3.Reflect( Direction, o.Contact.Normal ).Normal.WithX( 0 );
+		Direction = Flatten( Vector3.Reflect( Direction, o.Contact.Normal ) ).Normal;
 		o.Self.Body.Velocity = Direction * Speed;
 		o.Self.Body.AngularVelocity = 0;
 
 		if ( CollisionIndicator.IsValid() )
 		{
-			CollisionIndicator.Clone( o.Contact.Point);
+			CollisionIndicator.Clone( o.Contact.Point );
 		}
 	}
 }
